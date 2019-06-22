0703b02695c2d1341180b4c3afb27584bbd80159
Steve Hillier <stephenhillier@gmail.com>
2019-06-21 21:32:59 -0700

    refactor/tidy up backend

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -21,7 +21,7 @@ SERVER_NAME = os.getenv("SERVER_NAME")
 SERVER_HOST = os.getenv("SERVER_HOST")
 BACKEND_CORS_ORIGINS = os.getenv(
     "BACKEND_CORS_ORIGINS"
-)  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, http://local.dockertoolbox.tiangolo.com"
+)  # a string of origins separated by commas, e.g: "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080"
 PROJECT_NAME = os.getenv("PROJECT_NAME")
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 
