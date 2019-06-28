146501ba5dbebc2da5d926975a4361d9e79d1a5f
Steve Hillier <stephenhillier@gmail.com>
2019-06-27 17:01:58 -0700

    first cut at sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    show
+    app
+  >
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-btn
+              icon
+              @click.stop="mini = !mini"
+            >
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        @click="console.log('!!!')"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  components: {
+  },
+})
+export default class Sidebar extends Vue {
+  // initial data
+  drawer = false;
+  items = [
+    { title: 'Home', icon: 'dashboard' },
+    { title: 'About', icon: 'question_answer' }
+  ]
+  mini = true
+  right = null
+}
+</script>
