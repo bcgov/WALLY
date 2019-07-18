bb0e5a0d1b7532c06d6c8ee17e43e9f613a74d20
Steve Hillier <stephenhillier@gmail.com>
2019-07-18 13:47:02 -0700

    add a migrate step on rolling restart

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -4,7 +4,4 @@
 python /app/app/db/wait_for_db.py
 
 # Run migrations
-# alembic -c alembic/alembic.ini upgrade head
-
-# Create initial data in DB
-# python /app/app/initial_data.py
+alembic -c alembic/alembic.ini upgrade head
