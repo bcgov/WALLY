b821d307e207413aedec08cccb8544dc1c899a5f
AlexZorkin <alex@bigthink.solutions>
2019-07-26 13:56:38 -0700

    updated env variable for reporting service url

--- frontend/src/store/reportStore.js
+++ frontend/src/store/reportStore.js
@@ -1,6 +1,4 @@
-import EventBus from '../services/EventBus.js'
-import * as utils from '../utils/mapUtils'
-import ApiService from "../services/ApiService";
+import ApiService, {reportingServiceURL} from "../services/ApiService";
 
 export default {
   state: {
@@ -8,13 +6,13 @@ export default {
   },
   actions: {
     downloadFeatureReport ({ commit }, payload) {
-      ApiService.post('http://localhost:3000' + '/featureReport', payload, {responseType: 'arraybuffer'})
+      ApiService.post(reportingServiceURL + '/featureReport', payload, {responseType: 'arraybuffer'})
         .then((res) => {
         console.log(res)
         let blob = new Blob([res.data], { type: 'application/pdf' })
         let link = document.createElement('a')
         link.href = window.URL.createObjectURL(blob)
-        link.download = 'test.pdf'
+        link.download = 'WaterReport.pdf'
         link.click()
       }).catch((error) => {
         console.log(error)
