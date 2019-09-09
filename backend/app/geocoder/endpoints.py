6f3c15974443f0f964466084cb7de4131a49d980
Steve Hillier <stephenhillier@gmail.com>
2019-09-09 14:01:06 -0700

    shorten long lines

--- backend/app/geocoder/endpoints.py
+++ backend/app/geocoder/endpoints.py
@@ -10,8 +10,9 @@ logger = getLogger("geocoder")
 
 router = APIRouter()
 
-# Wally's web client uses https://github.com/mapbox/mapbox-gl-geocoder as a search/geocoding control.
-# To maintain compatibility with Mapbox geocoding, this endpoint has to handle several path parameters that we don't need (_1 and _2).
+# Wally's web client uses https://github.com/mapbox/mapbox-gl-geocoder as a
+# search/geocoding control. To maintain compatibility with Mapbox geocoding,
+# this endpoint has to handle several path parameters that we don't need (_1 and _2).
 # The q parameter includes `.json` after the query string, so we need to handle that as well.
 @router.get("/geocoding/{_1}/{_2}/{query}.json")
 def geocode_lookup(
