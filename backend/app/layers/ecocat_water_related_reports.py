aff3e7f62a2b4e1600918bd5a300bd986b066a03
AlexZorkin <alex@bigthink.solutions>
2019-09-09 16:54:27 -0700

    base layers

---
+++ backend/app/layers/ecocat_water_related_reports.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from sqlalchemy import Integer, String, Column, DateTime, BLOB
+from app.db.base_class import BaseTable
+from geoalchemy2 import Geometry
+
+
+class EcocatWaterRelatedReports(BaseTable):
+    __tablename__ = 'ecocat_water_related_reports'
+
+    REPORT_POINT_ID = Column(Integer, primary_key=True, comment='')
+    FEATURE_CODE = Column(String, comment='')
+    REPORT_ID = Column(Integer, comment='')
+    TITLE = Column(String, comment='')
+    SHORT_DESCRIPTION = Column(String, comment='')
+    AUTHOR = Column(String, comment='')
+    DATE_PUBLISHED = Column(DateTime, comment='')
+    WATERSHED_CODE = Column(String, comment='')
+    WATERBODY_IDENTIFIER = Column(String, comment='')
+    LONG_DESCRIPTION = Column(String, comment='')
+    REPORT_AUDIENCE = Column(String, comment='')
+    GEOMETRY = Column(Geometry, comment='')
+    OBJECTID = Column(Integer, comment='')
+    SE_ANNO_CAD_DATA = Column(BLOB, comment='')
