b0c0337ae785ca1b65a5e96b10d4ce6a018917ee
AlexZorkin <alex@bigthink.solutions>
2019-09-09 17:32:52 -0700

    more db layers added

---
+++ backend/app/layers/water_rights_licenses.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from sqlalchemy import Integer, String, Column, DateTime, BLOB, Float
+from app.db.base_class import BaseTable
+from geoalchemy2 import Geometry
+
+
+class WaterRightsLicenses(BaseTable):
+    __tablename__ = 'water_rights_licenses'
+
+
