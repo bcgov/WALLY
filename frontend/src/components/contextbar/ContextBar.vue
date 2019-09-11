bb9e73421c77e76659783aac3387cd8a1424edd9
Steve Hillier <stephenhillier@gmail.com>
2019-09-11 10:41:05 -0700

    revert context bar changes

--- frontend/src/components/contextbar/ContextBar.vue
+++ frontend/src/components/contextbar/ContextBar.vue
@@ -7,23 +7,23 @@
     </v-slide-x-transition>
     <v-slide-x-reverse-transition>
       <v-card id="contextBar" class="mx-auto" max-width="450">
-        <div fluid v-show="showContextBar" max-width="450">
-          <div>
+        <div v-show="showContextBar" max-width="450">
+          <v-card-actions>
             <v-btn small icon @click="toggleContextBar" class="minimizeContextBar" id="ContextButtonHide">
               <v-icon dark v-if="showContextBar">keyboard_arrow_right</v-icon>
             </v-btn>
-            <span id="componentsList">
-              <template v-for="(item, i) in contextComponents">
-                <v-card :key="i" min-width="400" class="component">
-                  <v-card-title>
-                    <span v-if="item && item">{{item.title}}</span>
-                  </v-card-title>
-                  <v-card-text>
-                    <component :is="item.component" v-bind="item.data" v-bind:key="item.key"></component>
-                  </v-card-text>
-                </v-card>
-              </template>
-            </span>
+          </v-card-actions>
+  <!--            <v-card-title>Context</v-card-title>-->
+          <span id="componentsList">
+          <v-card v-for="(item, i) in contextComponents" :key="i" min-width="400" class="component">
+            <v-card-title>
+              <span v-if="item && item">{{item.title}}</span>
+            </v-card-title>
+            <v-card-text>
+              <component :is="item.component" v-bind="item.data" v-bind:key="item.key"></component>
+            </v-card-text>
+          </v-card>
+          </span>>
           </div>
           <div v-if="!contextComponents.length" class="pa-3">
             <v-btn small icon @click="toggleContextBar" class="minimizeContextBar" id="ContextButtonHide">
@@ -33,8 +33,6 @@
               <p>Select a region using the rectangular tool or click on wells, aquifers, water licences and other features to display information.</p>
             </div>
           </div>
-
-        </div>
       </v-card>
     </v-slide-x-reverse-transition>
   </div>
