fe6eafbf8227daf23566c9fe2d395b7110446a36
Steve Hillier <stephenhillier@gmail.com>
2019-09-11 10:42:31 -0700

    fix tags

--- frontend/src/components/contextbar/ContextBar.vue
+++ frontend/src/components/contextbar/ContextBar.vue
@@ -13,26 +13,25 @@
               <v-icon dark v-if="showContextBar">keyboard_arrow_right</v-icon>
             </v-btn>
           </v-card-actions>
-  <!--            <v-card-title>Context</v-card-title>-->
           <span id="componentsList">
-          <v-card v-for="(item, i) in contextComponents" :key="i" min-width="400" class="component">
-            <v-card-title>
-              <span v-if="item && item">{{item.title}}</span>
-            </v-card-title>
-            <v-card-text>
-              <component :is="item.component" v-bind="item.data" v-bind:key="item.key"></component>
-            </v-card-text>
-          </v-card>
-          </span>>
+            <v-card v-for="(item, i) in contextComponents" :key="i" min-width="400" class="component">
+              <v-card-title>
+                <span v-if="item && item">{{item.title}}</span>
+              </v-card-title>
+              <v-card-text>
+                <component :is="item.component" v-bind="item.data" v-bind:key="item.key"></component>
+              </v-card-text>
+            </v-card>
+          </span>
+        </div>
+        <div v-if="!contextComponents.length" class="pa-3">
+          <v-btn small icon @click="toggleContextBar" class="minimizeContextBar" id="ContextButtonHide">
+            <v-icon dark v-if="showContextBar">keyboard_arrow_right</v-icon>
+          </v-btn>
+          <div class="mt-3">
+            <p>Select a region using the rectangular tool or click on wells, aquifers, water licences and other features to display information.</p>
           </div>
-          <div v-if="!contextComponents.length" class="pa-3">
-            <v-btn small icon @click="toggleContextBar" class="minimizeContextBar" id="ContextButtonHide">
-              <v-icon dark v-if="showContextBar">keyboard_arrow_right</v-icon>
-            </v-btn>
-            <div class="mt-3">
-              <p>Select a region using the rectangular tool or click on wells, aquifers, water licences and other features to display information.</p>
-            </div>
-          </div>
+        </div>
       </v-card>
     </v-slide-x-reverse-transition>
   </div>
