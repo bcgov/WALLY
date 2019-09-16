76edf5e0c68413d362a61cab70709a00ea74af1e
Steve Hillier <stephenhillier@gmail.com>
2019-09-16 10:46:48 -0700

    merge alembic migrations

---
+++ backend/alembic/versions/d0f00a33947e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d0f00a33947e
+Revises: db674c5861c7
+Create Date: 2019-09-16 17:45:56.402740
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd0f00a33947e'
+down_revision = 'db674c5861c7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
