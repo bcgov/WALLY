25ce4c1952567fa3423d4eae032667f0d2d1425a
Daine Trinidad <daine.trinidad@gov.bc.ca>
2019-09-18 16:47:02 -0700

    Cleanup

--- imports/download_geojson_info.sh
+++ imports/download_geojson_info.sh
@@ -2,10 +2,10 @@
 
 # Send a request for geoJSON layers from NRS Apps
 # Emails get sent to the email address on geojson_request.json
+# USE THIS WISELY
 
 payload_url="https://apps.gov.bc.ca/pub/dwds-ofi/public/order/createOrderFiltered/"
 
-test="hello"
 PAYLOAD=$(<geojson_request.json)
 command="curl -L -i -X POST -H 'Content-Type: application/json' -d '$PAYLOAD' $payload_url"
 echo $command
