ad7e50a470abcbf3c9fadbfd86f093a988750afe
Daine Trinidad <daine.trinidad@gov.bc.ca>
2019-09-19 09:45:23 -0700

    Updated the script

---
+++ imports/README.md
@@ -0,0 +1,9 @@
+# Import Scripts
+
+Downloading geojson files
+-------
+Shell script to download geojson files from the Data Catalogue
+
+```bash
+$ bash download_geojson_info.sh <email_address_to_send_to>
+``` 
--- imports/download_geojson_info.sh
+++ imports/download_geojson_info.sh
@@ -4,11 +4,31 @@
 # Emails get sent to the email address on geojson_request.json
 # USE THIS WISELY
 
-payload_url="https://apps.gov.bc.ca/pub/dwds-ofi/public/order/createOrderFiltered/"
+
+# Order 2031571
+# https://apps.gov.bc.ca/pub/dwds-rasp/pickup/2031571
+# https://distribution.data.gov.bc.ca/BCGW_7113060B_156885004189_7212.zip
+
+EMAIL=$1
+
+[ -z "$EMAIL" ] && echo "You need to provide an email address" && exit 1
+
+PAYLOAD_URL="https://apps.gov.bc.ca/pub/dwds-ofi/public/order/createOrderFiltered/"
 
 PAYLOAD=$(<geojson_request.json)
-command="curl -L -i -X POST -H 'Content-Type: application/json' -d '$PAYLOAD' $payload_url"
-echo $command
+PAYLOAD=${PAYLOAD/<email_address>/$EMAIL}
+COMMAND="curl -L -i -X POST -H 'Content-Type: application/json' -d '$PAYLOAD' $PAYLOAD_URL"
 
-eval $command
+echo "-------------------------------"
+echo $COMMAND
+echo "-------------------------------"
+
+while true; do
+    read -p "Run the command? " yn
+    case $yn in
+        [Yy]* ) eval $command; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
