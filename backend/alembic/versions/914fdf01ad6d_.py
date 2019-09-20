d8e68b27b0deec32220a70835e9a1f2bac8ed41f
AlexZorkin <alex@bigthink.io>
2019-09-20 14:30:48 -0700

    merge migrations

---
+++ backend/alembic/versions/914fdf01ad6d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 914fdf01ad6d
+Revises: 978751d9a7f2, 96815a5f7752
+Create Date: 2019-09-20 14:29:43.007044
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '914fdf01ad6d'
+down_revision = ('978751d9a7f2', '96815a5f7752')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
