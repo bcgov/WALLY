c6eccb653fe4f8cd1ac4e3fb8cd22ecfe47f3f53
Steve Hillier <stephenhillier@gmail.com>
2019-09-20 13:00:25 -0700

    limit hydat geocoder to BC stations

--- backend/alembic/versions/56cd94a2e609_update_search_view_add_filter.py
+++ backend/alembic/versions/56cd94a2e609_update_search_view_add_filter.py
@@ -1,8 +1,8 @@
-"""update search view
+"""update_search_view2
 
-Revision ID: 978751d9a7f2
-Revises: 73ffeef2cc38
-Create Date: 2019-09-18 19:36:46.534164
+Revision ID: 56cd94a2e609
+Revises: 978751d9a7f2
+Create Date: 2019-09-20 19:51:05.531222
 
 """
 from alembic import op
@@ -10,17 +10,13 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '978751d9a7f2'
-down_revision = '73ffeef2cc38'
+revision = '56cd94a2e609'
+down_revision = '978751d9a7f2'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    # replace materialized view with new view with extra records added.
-    # note on table names: some layers like water_rights_licenses do not have
-    # consistent spelling across
-    # the application and in external resources.
     op.execute("DROP MATERIALIZED VIEW IF EXISTS geocode_lookup")
     op.execute("""
         CREATE MATERIALIZED VIEW geocode_lookup AS
@@ -33,6 +29,7 @@ def upgrade():
         'hydrometric_stream_flow' AS layer,
         to_tsvector(concat_ws(' ',stn.station_number, stn.station_name)) AS tsv
         FROM hydat.stations AS stn
+        WHERE stn.prov_terr_state_loc = 'BC'
 
         UNION
         SELECT
@@ -54,11 +51,9 @@ def upgrade():
         to_tsvector(concat_ws(' ', wrl."POD_NUMBER"::text, wrl."LICENCE_NUMBER"::text, wrl."SOURCE_NAME")) AS tsv
         FROM water_rights_licenses AS wrl
     """)
-
     op.execute("""
         create index idx_geocode_tsv ON geocode_lookup USING GIN(tsv)
     """)
-    op.execute("drop table parcel")
 
 
 def downgrade():
