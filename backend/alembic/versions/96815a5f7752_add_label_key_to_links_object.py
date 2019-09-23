8ace3d3c988e954ff76e0cc6ed86d2e836d26ba3
AlexZorkin <alex@bigthink.solutions>
2019-09-23 15:13:51 -0700

    migration fix

--- backend/alembic/versions/96815a5f7752_add_label_key_to_links_object.py
+++ backend/alembic/versions/96815a5f7752_add_label_key_to_links_object.py
@@ -1,6 +1,8 @@
-  """add label key to links object
+"""add label key to links object
+
 Revision ID: 96815a5f7752
-Revises: 73ffeef2cc38
+Revises: 56cd94a2e609
+
 Create Date: 2019-09-19 15:49:17.627612
 """
 from alembic import op
@@ -9,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '96815a5f7752'
-down_revision = '516ece576828'
+down_revision = '56cd94a2e609'
 branch_labels = None
 depends_on = None
 
