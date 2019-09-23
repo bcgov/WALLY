ebe42feca6893998231f368c4092f2ec98b74d74
Daine Trinidad <daine.trinidad@gov.bc.ca>
2019-09-23 16:51:56 -0700

    Fixed test

--- frontend/tests/unit/MapLegend.spec.js
+++ frontend/tests/unit/MapLegend.spec.js
@@ -1,10 +1,37 @@
-import { shallowMount } from '@vue/test-utils'
-import Header from '../../src/components/map/MapLegend.vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+import MapLegend from '../../src/components/map/MapLegend.vue'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import Vue from 'vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+// localVue with Vuetify shows console warnings so we'll use Vue instead
+// https://github.com/vuetifyjs/vuetify/issues/4964
+// localVue.use(Vuetify)
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify()
 
 describe('Map Legend Test', () => {
+  let wrapper
+  let store
+  let getters
+
+  beforeEach(() => {
+    getters = {
+      activeMapLayers: () => []
+    }
+    store = new Vuex.Store({ getters })
+    wrapper = mount(MapLegend, {
+      vuetify,
+      store,
+      localVue
+    })
+  })
+
   it('Is hidden when empty', () => {
-    const wrapper = shallowMount(Header)
-    expect(wrapper.text()).toMatch('Water Allocation')
+    expect(wrapper.findAll('div').length).toBe(0)
   })
 
   it('Shows the legend div', () => {
