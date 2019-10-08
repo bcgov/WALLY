5a34f282f98a014bfbfa36598d47ce4c50e831d8
Daine Trinidad <daine.trinidad@gov.bc.ca>
2019-10-07 23:36:17 -0700

    WIP hanged to fileresponse

--- backend/app/utils/endpoints.py
+++ backend/app/utils/endpoints.py
@@ -2,7 +2,7 @@ import logging
 from fastapi import APIRouter, Depends, HTTPException
 import pdfkit
 from app import config
-from starlette.responses import Response
+from starlette.responses import FileResponse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("utils")
@@ -13,12 +13,19 @@ router = APIRouter()
 @router.get("/report")
 def generate_report():
     logger.info(f"getting report from {config.REPORT_URL}")
-    pdf_report = pdfkit.from_url(config.REPORT_URL, False)
 
-    logger.info(f"pdf: ", pdf_report)
+    temp_file = '/tmp/report.pdf'
+    pdfkit.from_url(config.REPORT_URL, temp_file)
 
-    response = Response(
-        content=pdf_report,
-        media_type='application/pdf',
-        headers={'Content-Disposition': 'attachment; filename=report.pdf'})
+    logger.info(f"Saved to {temp_file}")
+
+    response = FileResponse(
+        path=temp_file,
+        media_type="application/pdf",
+        filename='report.pdf')
+
+    # response = Response(
+    #     content=pdf_report,
+    #     media_type='application/pdf',
+    #     headers={'Content-Disposition': 'attachment; filename=report.pdf'})
     return response
