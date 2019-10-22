36b3594ecdd5abc8261765b6c92cb54aeef340f6
Steve Hillier <stephenhillier@gmail.com>
2019-10-22 14:25:12 -0700

    add calculated columns (SWL - top of screen)

--- frontend/src/components/analysis/DistanceToWells.vue
+++ frontend/src/components/analysis/DistanceToWells.vue
@@ -26,6 +26,16 @@
           <template v-slot:item.top_of_screen="{ item }">
             <span>{{calculateTopOfScreens(item.screen_set)}}</span>
           </template>
+          <template v-slot:item.swl_to_screen="{ item }">
+            <span v-if="item.screen_set && item.screen_set.length && item.static_water_level">
+              {{ calculateTopOfScreens(item.screen_set) - Number(item.static_water_level) }}
+            </span>
+          </template>
+          <template v-slot:item.swl_to_bottom_of_well="{ item }">
+            <span v-if="item.finished_well_depth && item.static_water_level">
+              {{  Number(item.finished_well_depth) - Number(item.static_water_level) }}
+            </span>
+          </template>
         </v-data-table>
       </v-col>
     </v-row>
@@ -55,7 +65,11 @@ export default {
       { text: 'Distance (m)', value: 'distance', align: 'right' },
       { text: 'Reported yield (USGPM)', value: 'well_yield', align: 'right' },
       { text: 'Static water level (ft)', value: 'static_water_level', align: 'right' },
-      { text: 'Top of screen (ft)', value: 'top_of_screen', align: 'right' }
+      { text: 'Top of screen (ft)', value: 'top_of_screen', align: 'right' },
+      { text: 'Finished well depth (ft)', value: 'finished_well_depth', align: 'right' },
+      { text: 'SWL to top of screen (ft)', value: 'swl_to_screen', align: 'right' },
+      { text: 'SWL to bottom of well (ft)', value: 'swl_to_bottom_of_well', align: 'right' }
+
     ]
   }),
   computed: {
