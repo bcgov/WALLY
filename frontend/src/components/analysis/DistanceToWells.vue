8b272333758e030663a091e294ae685bbab37145
Steve Hillier <stephenhillier@gmail.com>
2019-10-28 15:36:38 -0700

    added some explanations for fields

--- frontend/src/components/analysis/DistanceToWells.vue
+++ frontend/src/components/analysis/DistanceToWells.vue
@@ -26,6 +26,38 @@
         </v-data-table>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-expansion-panels class="mt-5 elevation-0" multiple>
+          <v-expansion-panel class="elevation-0">
+            <v-expansion-panel-header disable-icon-rotate class="grey--text text--darken-4 subtitle-1">
+              Where does this information come from?
+              <template v-slot:actions>
+                <v-icon color="primary">mdi-help-circle-outline</v-icon>
+              </template>
+
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <p>Data on this page comes from <a href="https://apps.nrs.gov.bc.ca/gwells/" target="_blank">Groundwater Wells and Aquifers</a>.</p>
+              <dl>
+                <dt>Reported yield</dt>
+                <dd>Estimated by the well driller during construction by conducting a well yield test. US gallons per minute.</dd>
+                <dt>Static water level</dt>
+                <dd>The level (from the top of the casing) to which water will naturally rise in a well without pumping, measured in feet (ft btoc).</dd>
+                <dt>Top of screen</dt>
+                <dd>The depth (from ground level) to the top of the uppermost reported screen segment. This figure is automatically calculated using data provided in construction reports.</dd>
+                <dt>Finished well depth</dt>
+                <dd>The depth at which the well was 'finished'. It can be shallower from the total well depth which is the total depth at which the well was drilled. The finished depth is represented in units of feet bgl (below ground level).</dd>
+                <dt>SWL to top of screen</dt>
+                <dd>The distance from the static water level to top of screen (see definition above).</dd>
+                <dt>SWL to bottom of well</dt>
+                <dd>The distance from the static water level to the finished well depth (see definition above).</dd>
+              </dl>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
