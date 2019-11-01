af654556d45e62d9b7e0e4d7402f378c0e50826f
Steve Hillier <stephenhillier@gmail.com>
2019-10-31 22:47:34 -0700

    fix mc cmd

--- openshift/import-jobs/databc/load_layer_data.sh
+++ openshift/import-jobs/databc/load_layer_data.sh
@@ -11,10 +11,10 @@
 set -e
 cd /dataload
 echo "Setting up Minio host"
-mc config host add minio http://minio:9000 "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+./mc config host add minio http://minio:9000 "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
 echo "Copying layer from Minio storage..."
-mc cp "minio/geojson/$1.zip" "./"
+./mc cp "minio/geojson/$1.zip" "./"
 
 echo "Loading data with ogr2ogr"
 ogr2ogr -f "PostgreSQL" PG:"host=$POSTGRES_SERVER port=5432 dbname=wally user=wally password=$POSTGRES_PASSWORD" "./$1.zip/$1.geojson" -nln $1 -t_srs EPSG:4326 -append -progress -skipfailures --config OGR_TRUNCATE YES --config PG_USE_COPY YES
