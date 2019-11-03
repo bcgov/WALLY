3cc5e3923fe1b440688c82b461406213f24bbf9b
Steve Hillier <stephenhillier@gmail.com>
2019-11-02 20:16:44 -0700

    working on order script

--- openshift/import-jobs/databc/create_order.sh
+++ openshift/import-jobs/databc/create_order.sh
@@ -13,9 +13,9 @@ set -e
 
 # temporary, just for testing
 EMAIL="gwells@gov.bc.ca"
-LAYER_NAME='WHSE_WATER_MANAGEMENT.GW_AQUIFERS_CLASSIFICATION_SVW'
-LAYER_DESC='Ground Water Aquifers'
-LAYER_URL='https://catalogue.data.gov.bc.ca/dataset/ground-water-aquifers'
+LAYER_NAME='WHSE_BASEMAPPING.FWA_STREAM_NETWORKS_SP'
+LAYER_DESC=''
+LAYER_URL=''
 PAYLOAD_URL="https://apps.gov.bc.ca/pub/dwds-ofi/public/order/createOrderFiltered/"
 
 PAYLOAD=$(<geojson_request.json)
@@ -24,17 +24,27 @@ PAYLOAD=${PAYLOAD/<layer_name>/$LAYER_NAME}
 PAYLOAD=${PAYLOAD/<layer_desc>/$LAYER_DESC}
 PAYLOAD=${PAYLOAD/<layer_url>/$LAYER_URL}
 
-curl -Ls -H 'Content-Type: application/json' --data "$PAYLOAD" $PAYLOAD_URL
+# curl -Ls -H 'Content-Type: application/json' --data "$PAYLOAD" $PAYLOAD_URL
 
-# ORDER_ID=$(curl -Ls -H 'Content-Type: application/json' --data "$PAYLOAD" $PAYLOAD_URL)
-# echo "$ORDER_ID"
-# ORDER_ID=$(echo $ORDER_ID | jq -r ".Value")
+ORDER_ID=$(curl -Ls -H 'Content-Type: application/json' --data "$PAYLOAD" $PAYLOAD_URL)
+echo "$ORDER_ID"
+ORDER_ID=$(echo $ORDER_ID | jq .Value)
 
-# echo "Order $ORDER_ID placed. Waiting for download to become available"
+(( $ORDER_ID+0 < 10000 )) && echo "$ORDER_ID" && exit 1
 
-# declare -a link
 
-# while true; do
-#   link=$(curl -s https://apps.gov.bc.ca/pub/dwds-ofi/order/2038490 | jq -r '.ORDER_DOWNLOAD_PATH')
-#   [ -z "$link" ] && break
-# done
+echo "Order $ORDER_ID placed. Waiting for download to become available"
+exit 1
+
+# script needs work past this point
+# needs to exit on error codes (e.g. value = 11, 16 etc. Value will be a 7 digit order # if successful)
+declare -a link
+
+while true; do
+  ((i++)) && ((i==300)) && echo "Exceeded loop limit ($i) without download link becoming available." && exit 1
+  sleep 15
+  echo "Checking status of order $ORDER_ID..."
+  link=$(curl -s https://apps.gov.bc.ca/pub/dwds-ofi/order/$ORDER_ID | jq -r '.ORDER_DOWNLOAD_PATH')
+  [ -z "$link" ] && echo "Order ready at $link" && break
+  echo "Order not ready yet. Checking again in 15 seconds..."
+done
