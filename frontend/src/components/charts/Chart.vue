6860dfe6662a3c1f43a3c29cc6e198b5b3284212
Daine Trinidad <daine.trinidad@gov.bc.ca>
2019-11-05 14:46:42 -0800

    Added chart wrapper for plotly

---
+++ frontend/src/components/charts/Chart.vue
@@ -0,0 +1,19 @@
+<template>
+  <Plotly :data="data" :layout="layout" :display-mode-bar="false" :key="4" class="chart"></Plotly>
+</template>
+<script>
+import { Plotly } from 'vue-plotly'
+
+export default {
+  name: 'Chart',
+  components: {
+    Plotly
+  },
+  props: {
+    data: Array,
+    layout: {
+      title: String
+    }
+  }
+}
+</script>
