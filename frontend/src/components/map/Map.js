8c166f7b10aec75f9a1e7f1c962ef363d8b356f6
AlexZorkin <alex@bigthink.io>
2019-11-19 08:59:30 -0800

    recursive function for downstream contacts

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -11,6 +11,7 @@ import circle from '@turf/circle'
 import * as metadata from '../../utils/metadataUtils'
 import coordinatesGeocoder from './localGeocoder'
 import bbox from '@turf/bbox'
+import _ from 'lodash';
 
 import qs from 'querystring'
 import ApiService from '../../services/ApiService'
@@ -28,6 +29,10 @@ const polygon = {
     'type': 'Polygon',
     'coordinates': [[]]
   }
+}
+const featureCollection = {
+  "type": "FeatureCollection",
+  "features": []
 }
 
 export default {
@@ -74,7 +79,8 @@ export default {
       // markerLayerGroup: L.layerGroup(),
       activeLayers: {},
       draw: null, // mapbox draw object (controls drawn polygons e.g. for area select)
-      isDrawingToolActive: false
+      isDrawingToolActive: false,
+      highlightedStream: {}
     }
   },
   computed: {
@@ -154,6 +160,7 @@ export default {
       }), 'top-right')
       this.map.on('style.load', () => {
         this.getMapLayers()
+        this.initStreamHighlights(this)
       })
 
       this.initHighlightLayers()
@@ -231,6 +238,160 @@ export default {
           }
         })
       })
+    },
+    initStreamHighlights() {
+      this.map.addSource('fwa_streams_data', { type: 'vector', "url": "mapbox://iit-water.6ihj1ke0" }) 
+      this.map.addLayer({
+          "id": "streams",
+          "type": "line",
+          "source": "fwa_streams_data",
+          "source-layer": "FWA_STREAM_NETWORKS_SP-bdhkfq",
+          "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#234075",
+              "line-width": 1
+          }
+      })
+      this.map.addSource('selectedStreamSource', { type: 'geojson', data: featureCollection })
+      this.map.addLayer({
+          "id": "selectedstream",
+          "type": "line",
+          "source": "selectedStreamSource",
+          "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#1500ff",
+              "line-width": 3
+          }
+      })
+      this.map.addSource('upstreamSource', { type: 'geojson', data: featureCollection })
+      this.map.addLayer({
+          "id": "upstream",
+          "type": "line",
+          "source": "upstreamSource",
+          "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#00ff26",
+              "line-width": 3
+          }
+      })
+      this.map.addSource('downstreamSource', { type: 'geojson', data: featureCollection }) 
+      this.map.addLayer({
+          "id": "downstream",
+          "type": "line",
+          "source": "downstreamSource",
+          "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          "paint": {
+              "line-color": "#ff4800",
+              "line-width": 3
+          }
+      })
+      this.map.on('mousemove', 'streams', this.updateStreamHighlight)
+      this.map.on('mouseleave', 'streams', function(e) {
+        var map = e.target.style.map
+        // map.getSource('upstreamSource').setData(featureCollection)
+        map.getCanvas().style.cursor = '';
+      })
+    },
+    updateStreamHighlight(e) {
+
+      // Change the cursor style as a UI indicator.
+        // this.map.getCanvas().style.cursor = 'pointer'
+        
+        // set map reference
+        // var map = e.target.style.map
+
+        // Single out the first found feature and check for duplicate
+        var stream = e.features[0]
+
+        // Check if we're hovering over the same stream segment
+        // We don't want to re-calculate when we don't have to
+        if(_.isEqual(this.highlightedStream.properties, stream.properties)) 
+        { return } else { this.highlightedStream = stream }
+
+        const watershedCode = stream.properties["FWA_WATERSHED_CODE"].replace(/-000000/g,'')
+
+        // Build our downstream code list
+        const codes = watershedCode.split("-")
+        var downstreamCodes = [codes[0]]
+        for (let i = 0; i < codes.length - 1; i++) {
+          downstreamCodes.push(downstreamCodes[i] + "-" + codes[i+1])
+        }
+        console.log(watershedCode)
+        console.log(stream.properties)
+        console.log(stream.properties["DOWNSTREAM_ROUTE_MEASURE"])
+        console.log(stream.properties["LOCAL_WATERSHED_CODE"])
+        // console.log(downstreamCodes)
+        console.log("**********************")
+      
+        // get all streams from layer
+        var streams = this.map.querySourceFeatures('fwa_streams_data', {
+          sourceLayer: 'FWA_STREAM_NETWORKS_SP-bdhkfq'
+        })
+
+        // loop streams to find matching cases for selected, upstream, and downstream conditions
+        let selectedFeatures = []
+        let upstreamFeatures = []
+        let downstreamFeatures = []
+        streams.forEach(stream => {
+          const code = stream.properties["FWA_WATERSHED_CODE"].replace(/-000000/g,'') // remove empty stream ids
+          if(code === watershedCode)  { selectedFeatures.push(stream) } // selected stream condition
+          if(code.includes(watershedCode) && code.length > watershedCode.length)  { upstreamFeatures.push(stream) } // up stream condition
+          if(downstreamCodes.indexOf(code) > -1 && code.length < watershedCode.length)  { downstreamFeatures.push(stream) } // down stream condition
+        })
+
+        let cleanedDownstreamFeatures = this.cleanDownStreams(downstreamFeatures, stream.properties["FWA_WATERSHED_CODE"])
+
+        // Insert the upstream data into the upstream data source
+        var upStreamCollection = Object.assign({}, featureCollection)
+        upStreamCollection["features"] = upstreamFeatures
+        this.map.getSource('upstreamSource').setData(upStreamCollection)
+
+        // Insert the selected stream data into the selected stream data source
+        var selectedStreamCollection = Object.assign({}, featureCollection)
+        selectedStreamCollection["features"] = selectedFeatures
+        this.map.getSource('selectedStreamSource').setData(selectedStreamCollection)
+
+        // Insert the downstream data into the downstream data source
+        var downStreamCollection = Object.assign({}, featureCollection) 
+        downStreamCollection["features"] = cleanedDownstreamFeatures
+        this.map.getSource('downstreamSource').setData(downStreamCollection)
+    },
+    cleanDownStreams(streams, code, builder = []) {
+      var segment = streams.find((s) => {
+        if(s.properties["LOCAL_WATERSHED_CODE"]) {
+          let local = s.properties["LOCAL_WATERSHED_CODE"]
+          let global = s.properties["FWA_WATERSHED_CODE"]
+          if(local === code && global !== local) {
+            return s
+          }
+        }
+      })
+      if(segment) {
+        let drm = segment.properties["DOWNSTREAM_ROUTE_MEASURE"]
+        let segmentCode = segment.properties["FWA_WATERSHED_CODE"]
+        let elements = streams.filter((f) => { 
+          if(f.properties["FWA_WATERSHED_CODE"] === segmentCode && 
+             f.properties["DOWNSTREAM_ROUTE_MEASURE"] < drm){
+              return f
+            }
+        })
+        builder = builder.concat(elements)
+        return this.cleanDownStreams(streams, segmentCode, builder)
+      } else {
+        return builder
+      }
     },
     polygonToolHelp () {
       const disableKey = 'disablePolygonToolHelp'
