b6fcfd56484e925d08649419c650839728069eaf
AlexZorkin <alex@bigthink.io>
2019-11-27 12:20:38 -0800

    chart structure

--- frontend/src/components/features/FeatureCrossSection.vue
+++ frontend/src/components/features/FeatureCrossSection.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <line-chart :chartData="featureData" :options="levelChartOptions"></line-chart>
 </template>
 <script>
 export default {
@@ -8,10 +8,54 @@ export default {
   },
   props: ['distance', 'elevations'],
   data: () => ({
+
   }),
   computed: {
-    
-  }
+    measurementData () {
+      if (!this.distance || !this.elevations) {
+        return { datasets: [] }
+      }
+      return {
+        datasets: [
+          {
+            label: 'Measurement Analysis',
+            lineTension: 0,
+            fill: false,
+            borderColor: '#1f548a',
+            data: this.elevations
+          }
+        ]
+      }
+    }
+  },
+  measurementOptions (title, units, yValues) {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            type: 'linear',
+            ticks: {
+              min: 0,
+              max: Math.floor(Math.max.apply(Math, yValues) + 1),
+              callback: function (value, index, values) {
+                return `${value} ${units}`
+              }
+            }
+          }],
+          xAxes: [{
+            type: 'linear',
+            ticks: {
+              min: 0,
+              max: 12,
+              callback: (value, index, values) => {
+                return SHORT_MONTHS[value - 1]
+              }
+            }
+          }]
+        }
+      }
+    }
 }
 </script>
 
