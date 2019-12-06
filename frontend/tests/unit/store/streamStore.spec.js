def4bdf09861d522278228156745b9cc952e5e3c
AlexZorkin <alex@bigthink.io>
2019-12-05 19:47:18 -0800

    temp tests comment

--- frontend/tests/unit/store/streamStore.spec.js
+++ frontend/tests/unit/store/streamStore.spec.js
@@ -5,19 +5,19 @@ import * as streamStore from '../../../src/store/streamStore'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe('Stream Store', () => {
-    let store
-    beforeEach(() => {
-        store = streamStore.default
-    })
+// describe('Stream Store', () => {
+//     let store
+//     beforeEach(() => {
+//         store = streamStore.default
+//     })
 
-    it('calculates upstream segments', () => {
+//     it('calculates upstream segments', () => {
 
-    })
-    it('calculates downstream segments', () => {
+//     })
+//     it('calculates downstream segments', () => {
 
-    })
-    it('calculates selectedstream segments', () => {
+//     })
+//     it('calculates selectedstream segments', () => {
 
-    })
-})
+//     })
+// })
