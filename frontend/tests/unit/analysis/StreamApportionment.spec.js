690114ab1cb1a9b5db93b6a82bd4ca9f4a86550b
daine <daine.trinidad@gov.bc.ca>
2019-12-17 11:04:41 -0800

    Added (and fixed) tests for stream apportionment

--- frontend/tests/unit/analysis/StreamApportionment.spec.js
+++ frontend/tests/unit/analysis/StreamApportionment.spec.js
@@ -1,8 +1,10 @@
 import { createLocalVue, mount } from '@vue/test-utils'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
-import StreamApportionment from '../../../src/components/analysis/StreamApportionment.vue'
+import flushPromises from 'flush-promises'
 import Vuex from 'vuex'
+
+import StreamApportionment from '../../../src/components/analysis/StreamApportionment.vue'
 import testStreams from './testStreams.json'
 
 const localVue = createLocalVue()
@@ -20,6 +22,7 @@ describe('Stream apportionment tests', () => {
   let getters
   let mutations
   let propsData
+
   beforeEach(() => {
     getters = {
       isMapLayerActive: state => layerId => false
@@ -32,14 +35,13 @@ describe('Stream apportionment tests', () => {
       vuetify,
       store,
       localVue,
-      /*
-       v-btn on mount cases an error
-         TypeError: Cannot read property '$scopedSlots' of undefined
-       so we're stubbing it
-       */
-      stubs: {
-        'v-btn': true
-      }
+      /* Issue with the sync mode in vue-test-utils
+      https://github.com/vuejs/vue-test-utils/issues/1130
+      Getting the error(s)
+        - TypeError: Cannot read property '$scopedSlots' of undefined
+        - TypeError: Cannot read property '$options' of undefined
+      */
+      sync: false
     })
   })
 
@@ -55,10 +57,12 @@ describe('Stream apportionment tests', () => {
     }
   }
 
-  it('Displays streams in a table', () => {
-    expect(wrapper.findAll('.v-card').length).toEqual(0)
+  it('Displays streams in a table', async () => {
+    expect(wrapper.findAll('tbody tr.v-data-table__empty-wrapper').length)
+      .toEqual(1)
     wrapper.setData({ streams: testStreams })
-    expect(wrapper.findAll('.v-card').length).toEqual(3)
+    await flushPromises()
+    expect(wrapper.findAll('tbody tr').length).toEqual(3)
   })
 
   it('Calculates apportionment', () => {
@@ -96,6 +100,47 @@ describe('Stream apportionment tests', () => {
     ]
     wrapper.setData({ streams: data })
     wrapper.vm.removeOverlaps()
+    expect(wrapper.vm.streams).toEqual(result)
+  })
+
+  it('Apportionment changes based on weighting factor', () => {
+    let data = [
+      { distance: 2, length_metre: 12.2 },
+      { distance: 4, length_metre: 12.2 }
+    ]
+    wrapper.setData({ streams: data, weightingFactor: 3 })
+    wrapper.vm.calculateApportionment()
+    expect(Math.round(wrapper.vm.streams[0]['apportionment']))
+      .toEqual(89)
+    expect(Math.round(wrapper.vm.streams[1]['apportionment']))
+      .toEqual(11)
+  })
+
+  it('Removes streams if apportionment is under x percentage', () => {
+    let data = [
+      { distance: 2, fwa_watershed_code: 5555, length_metre: 12.2 },
+      { distance: 4, fwa_watershed_code: 5555, length_metre: 12.2 },
+      { distance: 40, fwa_watershed_code: 5556, length_metre: 12.2 }
+    ]
+    let result = [
+      {
+        'apportionment': 80,
+        'distance': 2,
+        'length_metre': 12.2,
+        'fwa_watershed_code': 5555,
+        'inverse_distance': 0.25
+      },
+      {
+        'apportionment': 20,
+        'distance': 4,
+        'length_metre': 12.2,
+        'fwa_watershed_code': 5555,
+        'inverse_distance': 0.0625
+      }
+    ]
+    wrapper.setData({ streams: data })
+    wrapper.vm.calculateApportionment()
+    wrapper.vm.removeStreamsWithLowApportionment(10)
     expect(wrapper.vm.streams).toEqual(result)
   })
 })
