0d7a5b36292ace49f3d7d117810919f1511b9f7b
Steve Hillier <stephenhillier@gmail.com>
2019-12-17 16:02:16 -0800

    fix test API urls

--- frontend/src/components/features/FeatureWell.vue
+++ frontend/src/components/features/FeatureWell.vue
@@ -102,7 +102,7 @@ export default {
     },
     fetchWell () {
       this.wellLoading = true
-      ApiService.getRaw(`https://apps.nrs.gov.bc.ca/gwells/api/v1/wells/${this.id}`).then((r) => {
+      ApiService.getRaw(`https://gwells-dev-pr-1495.pathfinder.gov.bc.ca/gwells/api/v1/wells/${this.id}`).then((r) => {
         this.well = r.data
       }).catch((e) => {
         this.wellError = e
@@ -113,7 +113,7 @@ export default {
     fetchLicence () {
       this.licenceLoading = true
 
-      ApiService.getRaw(`https://apps.nrs.gov.bc.ca/gwells/api/v1/wells/licensing?well_tag_number=${this.id}`).then((r) => {
+      ApiService.getRaw(`https://gwells-dev-pr-1495.pathfinder.gov.bc.ca/gwells/api/v1/wells/licensing?well_tag_number=${this.id}`).then((r) => {
         this.well_licence = r.data
       }).catch((e) => {
         this.licence_error = e
