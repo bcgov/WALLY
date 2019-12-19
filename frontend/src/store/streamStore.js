a55b4c68b7e5e2c4dd73259269ecd302fff90dd3
Steve Hillier <stephenhillier@gmail.com>
2019-12-19 09:28:13 -0800

    more lint fixes

--- frontend/src/store/streamStore.js
+++ frontend/src/store/streamStore.js
@@ -6,8 +6,8 @@ import _ from 'lodash'
 export default {
   state: {
     featureCollection: {
-      "type": "FeatureCollection",
-      "features": []
+      'type': 'FeatureCollection',
+      'features': []
     },
     streamSources: config.sources,
     streamLayers: config.layers,
@@ -19,11 +19,10 @@ export default {
     selectedStreamBufferData: {}
   },
   actions: {
-    fetchConnectedStreams({commit, dispatch}, payload) {
+    fetchConnectedStreams ({ commit, dispatch }, payload) {
       // NOTE this action is the server backed query but at this point is too slow to implement
-      let fwaCode = payload.stream.properties["FWA_WATERSHED_CODE"]
+      let fwaCode = payload.stream.properties['FWA_WATERSHED_CODE']
       let outflowCode = fwaCode.substring(0, fwaCode.indexOf('-') + 1)
-      const watershedCode = fwaCode.replace(/-000000/g,'')
       ApiService.query(`/api/v1/analysis/stream/connections?outflowCode=${outflowCode}`)
         .then((response) => {
           console.log(response.data)
@@ -37,16 +36,15 @@ export default {
           console.log(error)
         })
     },
-    calculateStreamHighlights({commit, dispatch}, payload) {
-
+    calculateStreamHighlights ({ commit, dispatch }, payload) {
       // Get slected watershed code and trim un-needed depth
-      const watershedCode = payload.stream.properties["FWA_WATERSHED_CODE"].replace(/-000000/g,'')
+      const watershedCode = payload.stream.properties['FWA_WATERSHED_CODE'].replace(/-000000/g, '')
 
       // Build our downstream code list
-      const codes = watershedCode.split("-")
+      const codes = watershedCode.split('-')
       var downstreamCodes = [codes[0]]
       for (let i = 0; i < codes.length - 1; i++) {
-        downstreamCodes.push(downstreamCodes[i] + "-" + codes[i+1])
+        downstreamCodes.push(downstreamCodes[i] + '-' + codes[i + 1])
       }
 
       // loop streams to find matching cases for selected, upstream, and downstream conditions
@@ -54,23 +52,23 @@ export default {
       let upstreamFeatures = []
       let downstreamFeatures = []
       payload.streams.forEach(stream => {
-        const code = stream.properties["FWA_WATERSHED_CODE"].replace(/-000000/g,'') // remove empty stream ids
-        if(code === watershedCode)  { selectedFeatures.push(stream) } // selected stream condition
-        if(code.includes(watershedCode) && code.length > watershedCode.length)  { upstreamFeatures.push(stream) } // up stream condition
-        if(downstreamCodes.indexOf(code) > -1 && code.length < watershedCode.length)  { downstreamFeatures.push(stream) } // down stream condition
+        const code = stream.properties['FWA_WATERSHED_CODE'].replace(/-000000/g, '') // remove empty stream ids
+        if (code === watershedCode) { selectedFeatures.push(stream) } // selected stream condition
+        if (code.includes(watershedCode) && code.length > watershedCode.length) { upstreamFeatures.push(stream) } // up stream condition
+        if (downstreamCodes.indexOf(code) > -1 && code.length < watershedCode.length) { downstreamFeatures.push(stream) } // down stream condition
       })
 
       // Clean out downstream features that are upwards water flow
       // TODO may want to toggle this based on user feedback
-      dispatch('cleanDownStreams', { streams: downstreamFeatures, code: payload.stream.properties["FWA_WATERSHED_CODE"] })
+      dispatch('cleanDownStreams', { streams: downstreamFeatures, code: payload.stream.properties['FWA_WATERSHED_CODE'] })
 
       commit('setUpStreamData', upstreamFeatures)
       commit('setSelectedStreamData', selectedFeatures)
       // commit('setDownStreamData', cleanedDownstreamFeatures)
     },
-    cleanDownStreams({ commit, dispatch }, payload) {
+    cleanDownStreams ({ commit, dispatch }, payload) {
       let builder = payload.builder
-      if(!builder) {
+      if (!builder) {
         builder = []
       }
       // This is a recursive function that walks down the stream network
@@ -80,22 +78,22 @@ export default {
       // The BigO of this function is linear with a max of apprx. 50 due
       // to the max magnitude of a stream
       var segment = payload.streams.find((s) => {
-        if(s.properties["LOCAL_WATERSHED_CODE"]) {
-          let local = s.properties["LOCAL_WATERSHED_CODE"]
-          let global = s.properties["FWA_WATERSHED_CODE"]
-          if(local === payload.code && global !== local) {
+        if (s.properties['LOCAL_WATERSHED_CODE']) {
+          let local = s.properties['LOCAL_WATERSHED_CODE']
+          let global = s.properties['FWA_WATERSHED_CODE']
+          if (local === payload.code && global !== local) {
             return s
           }
         }
       })
-      if(segment) {
-        let drm = segment.properties["DOWNSTREAM_ROUTE_MEASURE"]
-        let segmentCode = segment.properties["FWA_WATERSHED_CODE"]
-        let elements = payload.streams.filter((f) => { 
-          if(f.properties["FWA_WATERSHED_CODE"] === segmentCode && 
-            f.properties["DOWNSTREAM_ROUTE_MEASURE"] < drm){
-              return f
-            }
+      if (segment) {
+        let drm = segment.properties['DOWNSTREAM_ROUTE_MEASURE']
+        let segmentCode = segment.properties['FWA_WATERSHED_CODE']
+        let elements = payload.streams.filter((f) => {
+          if (f.properties['FWA_WATERSHED_CODE'] === segmentCode &&
+            f.properties['DOWNSTREAM_ROUTE_MEASURE'] < drm) {
+            return f
+          }
         })
         builder = builder.concat(elements)
         // Recursive call step with current builder object and next segment selection
@@ -107,18 +105,18 @@ export default {
   },
   mutations: {
     setUpStreamData (state, payload) {
-      let collection = Object.assign({}, state.featureCollection) 
-      collection["features"] = _.unionBy(payload, state.upStreamData.features, x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0]) 
+      let collection = Object.assign({}, state.featureCollection)
+      collection['features'] = _.unionBy(payload, state.upStreamData.features, x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0])
       state.upStreamData = collection
     },
     setDownStreamData (state, payload) {
       let collection = Object.assign({}, state.featureCollection)
-      collection["features"] = _.unionBy(payload, state.downStreamData.features,  x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0])
+      collection['features'] = _.unionBy(payload, state.downStreamData.features, x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0])
       state.downStreamData = collection
     },
     setSelectedStreamData (state, payload) {
-      let collection = Object.assign({}, state.featureCollection) 
-      collection["features"] = _.unionBy(payload, state.selectedStreamData.features,  x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0])
+      let collection = Object.assign({}, state.featureCollection)
+      collection['features'] = _.unionBy(payload, state.selectedStreamData.features, x => x.properties.LINEAR_FEATURE_ID + x.geometry.coordinates[0])
       state.selectedStreamData = collection
     },
     resetStreamData (state) {
