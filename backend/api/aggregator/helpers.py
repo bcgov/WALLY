c6712514fd3c955d88502bde2333d5a9aa9a76b2
Steve Hillier <stephenhillier@gmail.com>
2019-12-18 16:44:15 -0800

    test with GWELLS staging

--- backend/api/aggregator/helpers.py
+++ backend/api/aggregator/helpers.py
@@ -27,7 +27,7 @@ def gwells_api_request(within):
     creates an ExternalAPIRequest object with params for accessing data from the
     GWELLS API.
     """
-    url = 'https://gwells-dev-pr-1495.pathfinder.gov.bc.ca/gwells/api/v1/wells/locations'
+    url = 'https://gwells-staging.pathfinder.gov.bc.ca/gwells/api/v2/wells/locations'
     params = GWELLSAPIParams(
         within=json.dumps(mapping(within)),
         geojson="true"
