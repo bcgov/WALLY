efdaf2356cd03d1ab7ede4783277253ec68e9824
AlexZorkin <alex@bigthink.solutions>
2020-01-28 10:28:43 -0800

    placeholder migration

---
+++ backend/alembic/versions/8d26b7e18b0f_add_iso_model_layer.py
@@ -0,0 +1,24 @@
+"""add iso model layer
+
+Revision ID: 8d26b7e18b0f
+Revises: 9ee500c7ad08
+Create Date: 2020-01-28 10:27:36.012094
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8d26b7e18b0f'
+down_revision = '9ee500c7ad08'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
