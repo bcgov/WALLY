c03936ed5fa70fd2062ad655f4a746504fbc2969
AlexZorkin <alex@bigthink.solutions>
2020-01-29 16:22:46 -0800

    type fix

--- backend/api/v1/isolines/controller.py
+++ backend/api/v1/isolines/controller.py
@@ -8,7 +8,7 @@ from api.v1.aggregator.helpers import transform_4326_3005
 logger = logging.getLogger("isolines")
 
 
-def calculate_runnoff_in_area(db: Session, polygon: MultiPolygon) -> float:
+def calculate_runnoff_in_area(db: Session, polygon: MultiPolygon):
     """
     Calculates the precipitation runoff using the area of `polygon` which intersects with features from
     the normal annual runoff isolines layer.
@@ -16,8 +16,11 @@ def calculate_runnoff_in_area(db: Session, polygon: MultiPolygon) -> float:
 
     isolines_layer = 'normal_annual_runoff_isolines'
 
-    isoline_features = feature_search(db, [isolines_layer], polygon)[
-        0].geojson.features
+    features = feature_search(db, [isolines_layer], polygon)
+    if features:
+      isoline_features = features[0].geojson.features
+    else: 
+      isoline_features = []
 
     area_total = 0
     runoff_total = 0
