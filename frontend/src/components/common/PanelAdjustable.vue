ec063adaa30728aa7fb01ff791971203f79a54c6
daine <daine.trinidad@gov.bc.ca>
2020-02-20 15:54:15 -0800

    Trying out a fixed panel

---
+++ frontend/src/components/common/PanelAdjustable.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app-overlay" v-if="this.showOverlay">
+    <InfoSheetAdjustable :panelName=this.panelName>
+      <router-view></router-view>
+    </InfoSheetAdjustable>
+  </div>
+</template>
+<style lang="scss">
+  #app-overlay{
+  }
+</style>
+<script>
+import { mapGetters } from 'vuex'
+import InfoSheet from './InfoSheetResponsive'
+import InfoSheetAdjustable from './InfoSheetAdjustable'
+
+export default {
+  name: 'PanelAdjustable',
+  components: {
+    InfoSheetAdjustable
+  },
+  props: {
+  },
+  data: () => {
+    return {
+      // panelWidth: 500
+    }
+  },
+  watch: {
+    $route (to, from) {
+      this.$store.dispatch('map/resizeMap')
+      console.log('route change', to, from )
+    }
+  },
+  computed: {
+    ...mapGetters('map', [
+      'isMapLayerActive',
+      'allMapLayers',
+      'mapLayerName',
+      'getMapLayer',
+      'getCategories',
+      'layerSelectionActive',
+      'isMapReady'
+
+    ]),
+    ...mapGetters([
+      'isDataMartActive',
+      'loadingFeature',
+      'featureError',
+      'dataMartFeatures',
+      'dataMartFeatureInfo',
+      'selectionBoundingBox',
+      'singleSelectionFeatures',
+      'loadingMultipleFeatures',
+      'featureSelectionExists'
+    ]),
+    panelName () {
+      if (this.isSingleSelectedFeature) {
+        return 'Feature Info'
+      } else if (this.isMultipleSelectedFeatures) {
+        return 'Features'
+      }
+      return 'Expand'
+    },
+    showOverlay () {
+      // Hide overlay side panel on initial load
+      return this.$route.path !== '/'
+    },
+    panelWidth () {
+      return 800
+    },
+    infoSheetComponent () {
+      return this.$store.state.feature.adjustableSidePanel ? InfoSheetAdjustable : InfoSheet
+    },
+    isFlexBoxComponent () {
+      return !this.$store.state.feature.adjustableSidePanel
+    },
+    isSingleSelectedFeature () {
+      return this.dataMartFeatureInfo && this.dataMartFeatureInfo.display_data_name
+    },
+    isMultipleSelectedFeatures () {
+      return this.dataMartFeatures && this.dataMartFeatures.length
+    }
+  },
+  mounted () {
+    console.log(this.$route)
+    console.log('panel mounted?')
+    // this.$store.dispatch('map/resizeMap')
+  },
+  methods: {
+  }
+}
+</script>
