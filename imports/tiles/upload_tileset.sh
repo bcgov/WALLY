4c8bd9874a0f0a70d441c9cecb050b48719797db
AlexZorkin <alex@bigthink.solutions>
2020-05-07 12:54:52 -0700

    upload script tweak

--- imports/tiles/upload_tileset.sh
+++ imports/tiles/upload_tileset.sh
@@ -9,10 +9,7 @@ mapbox_layer_id="$1"
 declare -a layers=($(psql -X -A -t "postgres://wally:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/wally" \
   --single-transaction \
   --field-separator=' ' \
-  -c "SELECT ds.data_table_name
-      FROM metadata.display_catalogue AS dc
-      JOIN metadata.data_source AS ds ON dc.data_source_id = ds.data_source_id
-      WHERE dc.mapbox_source_id='$1';"))
+  -c "SELECT display_data_name FROM metadata.display_catalogue WHERE mapbox_layer_id = '$1';"))
 
 [ "${layers[@]}" = ""] && echo "no layers found for tileset $1. Check the mapbox_source_id value for metadata.display_catalogue" && exit 1
 
@@ -36,7 +33,6 @@ echo "Copying mbtileset to Minio storage..."
 mc --config-dir=./.mc cp "./$mapbox_layer_id.mbtiles" "minio/tilestaging/$mapbox_layer_id.mbtiles"
 
 echo "Uploading $mapbox_layer_id.mbtiles to Mapbox using $mapbox_layer_id"
-echo "token: $MAPBOX_UPLOAD_TOKEN"
 mapbox --access-token "$MAPBOX_UPLOAD_TOKEN" upload "$mapbox_layer_id" "$mapbox_layer_id.mbtiles"
 
 echo "Finished."
