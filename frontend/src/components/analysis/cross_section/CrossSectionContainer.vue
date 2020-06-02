93b8f5ec09e262eef4851f7ddba4bcbbf24a5213
daine <daine.trinidad@gov.bc.ca>
2020-06-02 12:09:43 -0700

    New instructions. Moved instructions to reusable component

--- frontend/src/components/analysis/cross_section/CrossSectionContainer.vue
+++ frontend/src/components/analysis/cross_section/CrossSectionContainer.vue
@@ -31,21 +31,12 @@
 
     <v-row class="pa-5" v-else>
       <v-col cols=12 lg=10>
-        <v-card outlined>
+        <v-card>
           <v-card-title>Instructions</v-card-title>
           <v-card-text>
-            <p>Zoom into a place of interest on the map.</p>
-            <p>Click on the "Draw a Line" button and draw a line on the map, which can be a straight line through an
-              area of wells or can be segmented to connect different wells. Double click to complete the line.
-              The buffer radius is automatically set to 200 metres and can be updated to 0 to 1000 metres after
-              your analysis is produced.</p>
-            <p>If no analysis is produced, then go to the drop down Selection menu and "Reset Selections" and try again. It also helps to have the map zoomed to a smaller area with a sufficiently large amount of detail.</p>
-            <p>When you hover over the resulting 2D and 3D graphs, a toolbar of icons will appear. To view lithology, select the box or lasso icon and then create a box or lasso over the wells in the graph that you want to see the lithology for.</p>
-            <p>The table below the graph displays the wells in your buffer radius. If you do not want a well included in your analysis, then click on the trash icon in the corresponding row and it will be removed from the graph and analysis.</p>
-            <p>Select the Excel button to download the data and information related to the wells within your cross section.</p>
+            <CrossSectionInstructions></CrossSectionInstructions>
           </v-card-text>
         </v-card>
-<!--        <p>Draw a line to plot a cross section of subsurface data.</p>-->
         <div class="caption" v-if="!isWellsLayerEnabled"><a href="#" @click.prevent="enableWellsLayer">Enable groundwater wells map layer</a></div>
       </v-col>
       <v-col class="text-right" lg=2>
@@ -59,9 +50,11 @@
 import { mapGetters, mapActions } from 'vuex'
 
 import WellsCrossSection from './WellsCrossSection'
+import CrossSectionInstructions from './CrossSectionInstructions'
 export default {
   name: 'CrossSectionContainer',
   components: {
+    CrossSectionInstructions,
     WellsCrossSection
   },
   data: () => ({
