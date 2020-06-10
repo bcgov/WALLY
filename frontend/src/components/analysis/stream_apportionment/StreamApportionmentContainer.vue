2ca9b447de5df724ff6c3b6eeefc4dd940299f93
Steve Hillier <stephenhillier@gmail.com>
2020-06-10 13:43:46 -0700

    update instruction text

--- frontend/src/components/analysis/stream_apportionment/StreamApportionmentContainer.vue
+++ frontend/src/components/analysis/stream_apportionment/StreamApportionmentContainer.vue
@@ -36,7 +36,31 @@
     </div>
 
     <v-row class="pa-5" v-else>
-      <v-col cols=12 lg=8><p>Select a point of interest (e.g. a proposed well site) to apportion demand to nearby streams.</p></v-col>
+      <v-col cols=12 lg=8>
+        <p>Zoom into an area of interest on the map.</p>
+        <p>
+          Click the “Draw Point” button and drop a point of interest to represent a groundwater point of diversion.
+          The weighting factor is automatically set to 2 and can be updated after your analysis is produced.
+          The weighting factor must be either 1 (linear) or 2 (squared).
+        </p>
+        <p>
+          After your analysis is produced, there are buttons available to: remove multiple streams, remove overlaps,
+          and remove streams where apportionment is less than 10%. Each row in the table has an eye icon to highlight
+          stream segments and a trash can icon to delete that specific stream segment. If you have made modifications
+          to your analysis, a counterclockwise arrow icon will appear beside the weighting factor - click on this
+          button to reset the analysis to the initial results.
+        </p>
+        <p>
+          You can export results to Excel by selecting the “Excel” button.
+        </p>
+          If no analysis is produced, then go to the drop down Selection menu in the top navigation bar and
+          "Reset Selections" and try again. It also helps to have the map zoomed to a smaller area with a
+          sufficiently large amount of detail.
+        <p>
+          For more information on what data is used and caveats to the analysis, please review the
+          "Where does this information come from?" box at the bottom of the page.
+        </p>
+      </v-col>
       <v-col class="text-right"><v-btn @click="selectPoint" color="primary" outlined>Draw point</v-btn></v-col>
     </v-row>
   </v-container>
--- frontend/src/components/sidepanel/cards/UpstreamDownstream.vue
+++ frontend/src/components/sidepanel/cards/UpstreamDownstream.vue
@@ -22,6 +22,33 @@
     <v-row class="mx-3 mt-4">
       <v-col cols=12 lg=8>
         <p>Select a point on a stream to view water data upstream and downstream.</p>
+
+        <p>
+          Zoom into an area of interest on the map.
+        </p>
+        <p>
+          Click on the "Select Point" button and select a point on a stream. The buffer radius is
+          automatically set to 50 metres and can be updated from 0 to 500 metres after your analysis is produced.
+        </p>
+        <p>
+          You can search the entire upstream catchment or within the distance of the stream. The entire
+          upstream catchment is the full area that drains to the point you selected (entire drainage basin
+          upstream from your point). The stream buffer shows the area within the buffer radius from the stream.
+        </p>
+        <p>
+          A drop down menu gives you options of layers to analyze: Groundwater Wells, Water Rights Licences,
+          Water Rights Applications, EcoCat Reports, Aquifers or Critical Habitats. After you make a selection,
+          there is the option to enable the layer to see all the features of that layer.
+        </p>
+        <p>
+          If no analysis is produced, then go to the drop down Selection menu in the top navigation bar and
+          "Reset Selections" and try again. It also helps to have the map zoomed to a smaller area with a
+          sufficiently large amount of detail.
+        </p>
+        <p>
+          For more information on what data is used and caveats to the analysis, please review the "Where does
+          this information come from?" box at the bottom of the page.
+        </p>
       </v-col>
       <v-col class="text-right">
         <v-btn class="ml-3" @click="selectPoint" color="primary" outlined :disabled="buttonClicked">Select Point</v-btn>
