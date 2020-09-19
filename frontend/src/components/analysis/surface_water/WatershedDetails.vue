4b6f8596d6b39c56bbb531f8a88f969015f9f1d2
daine <daine.trinidad@gov.bc.ca>
2020-09-18 17:43:56 -0700

    Changes to watershed details design

--- frontend/src/components/analysis/surface_water/WatershedDetails.vue
+++ frontend/src/components/analysis/surface_water/WatershedDetails.vue
@@ -48,41 +48,46 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-card flat >
-          <v-card-title>
-            Mean Annual Discharge
-            <Dialog v-bind="wmd.meanAnnualDischarge"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="modelOutputs.mad">
-              {{ modelOutputs.mad }}
-              <strong>m³/s</strong>
-            </span>
-            <span v-else>
-              {{ noValueText }}
-            </span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat >
+            <v-card-title>
+              Mean Annual Discharge
+              <Dialog v-bind="wmd.meanAnnualDischarge"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="modelOutputs.mad">
+                {{ modelOutputs.mad }}
+                <strong>m³/s</strong>
+              </span>
+              <span v-else>
+                {{ noValueText }}
+              </span>
+            </v-card-text>
+          </v-card>
+        </v-col>
         <v-divider vertical></v-divider>
-        <v-card flat >
-          <v-card-title>
-            Total Annual Quantity
-            <Dialog v-bind="wmd.totalAnnualQuantity"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="availabilityPlotData">
-              {{ this.availabilityPlotData.reduce((a, b) => a + b, 0)
-                                          .toLocaleString('en', {maximumFractionDigits: 0}) }}
-              <strong>m³</strong>
-            </span>
-            <span v-else>
-              {{ noValueText }}
-            </span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat >
+            <v-card-title>
+              Total Annual Quantity
+              <Dialog v-bind="wmd.totalAnnualQuantity"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="availabilityPlotData">
+                {{ this.availabilityPlotData.reduce((a, b) => a + b, 0)
+                                            .toLocaleString('en', {maximumFractionDigits: 0}) }}
+                <strong>m³</strong>
+              </span>
+              <span v-else>
+                {{ noValueText }}
+              </span>
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
       <v-divider></v-divider>
       <v-row>
+        <v-col>
         <v-card flat >
           <v-card-title>
             Mean Annual Runoff
@@ -98,79 +103,88 @@
             <span v-else>{{ noValueText }}</span>
           </v-card-text>
         </v-card>
+        </v-col>
         <v-divider vertical></v-divider>
-        <v-card flat >
-          <v-card-title>
-            Low7Q2
-            <Dialog v-bind="wmd.low7Q2"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="modelOutputs.low7q2">
-              {{ modelOutputs.low7q2 }}
-              <strong>m³</strong>
-            </span>
-            <span v-else>{{ noValueText }}</span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat>
+            <v-card-title>
+              Low7Q2
+              <Dialog v-bind="wmd.low7Q2"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="modelOutputs.low7q2">
+                {{ modelOutputs.low7q2 }}
+                <strong>m³</strong>
+              </span>
+              <span v-else>{{ noValueText }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
         <v-divider vertical></v-divider>
-
-        <v-card flat >
-          <v-card-title>
-            Dry7Q10
-            <Dialog v-bind="wmd.dry7Q10"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="modelOutputs.dry7q10">
-              {{ modelOutputs.dry7q10 }}
-              <strong>m³/s</strong>
-            </span>
-            <span v-else>{{ noValueText }}</span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat >
+            <v-card-title>
+              Dry7Q10
+              <Dialog v-bind="wmd.dry7Q10"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="modelOutputs.dry7q10">
+                {{ modelOutputs.dry7q10 }}
+                <strong>m³/s</strong>
+              </span>
+              <span v-else>{{ noValueText }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
       <v-divider></v-divider>
       <v-row>
-        <v-card flat >
-          <v-card-title>
-            Annual Precipitation
-            <Dialog v-bind="wmd.annualPrecipitation"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="watershedDetails.annual_precipitation">
-              {{ watershedDetails.annual_precipitation.toFixed(0) }}
-              <strong>mm</strong>
-            </span>
-            <span v-else>{{ noValueText }}</span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat>
+            <v-card-title>
+              Annual Precipitation
+              <Dialog v-bind="wmd.annualPrecipitation"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="watershedDetails.annual_precipitation">
+                {{ watershedDetails.annual_precipitation.toFixed(0) }}
+                <strong>mm</strong>
+              </span>
+              <span v-else>{{ noValueText }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
         <v-divider vertical></v-divider>
-        <v-card flat >
-          <v-card-title>
-            Glacial Coverage
-            <Dialog v-bind="wmd.glacialCoverage"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="watershedDetails.glacial_coverage">
-              {{ watershedDetails.glacial_coverage.toFixed(2) }}
-            </span>
-            <span v-else>{{ noValueText }}</span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat >
+            <v-card-title>
+              Glacial Coverage
+              <Dialog v-bind="wmd.glacialCoverage"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="watershedDetails.glacial_coverage">
+                {{ watershedDetails.glacial_coverage.toFixed(2) }}
+              </span>
+              <span v-else>{{ noValueText }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
         <v-divider vertical></v-divider>
-
-        <v-card flat >
-          <v-card-title>
-            Median Elevation
-            <Dialog v-bind="wmd.medianElevation"/>
-          </v-card-title>
-          <v-card-text class="info-blue">
-            <span v-if="watershedDetails.median_elevation">
-              {{ watershedDetails.median_elevation.toFixed(0) }}
-              <strong>mASL</strong>
-            </span>
-            <span v-else>{{ noValueText }}</span>
-          </v-card-text>
-        </v-card>
+        <v-col>
+          <v-card flat >
+            <v-card-title>
+              Median Elevation
+              <Dialog v-bind="wmd.medianElevation"/>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <span v-if="watershedDetails.median_elevation">
+                {{ watershedDetails.median_elevation.toFixed(0) }}
+                <strong>mASL</strong>
+              </span>
+              <span v-else>{{ noValueText }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
       </v-row>
       <v-row>
         <v-card-actions>
