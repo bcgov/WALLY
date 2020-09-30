258c776c1b59c14a8dc2afbaaab2ef611e230249
daine <daine.trinidad@gov.bc.ca>
2020-09-30 13:45:21 -0700

    Added some tests (not yet passing)

--- frontend/tests/unit/sidebar/ImportLayer.spec.js
+++ frontend/tests/unit/sidebar/ImportLayer.spec.js
@@ -59,4 +59,39 @@ describe('ImportLayer', () => {
     expect(wrapper.vm.determineFileType('test.zip')).toBe('shp')
     expect(wrapper.vm.determineFileType('test.json')).toBe('geojson')
   })
+
+  it('Only accepts valid file types', () => {
+    wrapper.vm.fileList = []
+
+    let testFile = new File([''],
+      'test.geojson',
+      { type: 'application/geo+json' })
+
+    wrapper.vm.readFile(testFile)
+    expect(wrapper.find('v-alert').html().toLowerCase())
+      .toContain('unsupported file type')
+  })
+
+  it('Can load multiple files', () => {
+    wrapper.vm.fileList = []
+    let testFile = new File([''], 'test.geojson', { type: 'application/geo+json' })
+    let testFile2 = new File([''], 'test2.geojson', { type: 'application/geo+json' })
+    wrapper.vm.fileList = [testFile, testFile2]
+
+    expect(wrapper.vm.readFiles).toHaveBeenCalled()
+    expect(wrapper.vm.files.length).toBe(2)
+  })
+
+  it('Shows warning for files larger than 10mb', () => {
+    expect(1).toEqual(1)
+    let file = {
+      stats: {
+        size: 10 * 1e6
+      }
+    }
+    wrapper.vm.files = [file]
+
+    expect(wrapper.find('v-alert').html().toLowerCase())
+      .toContain('file size greater than 10mb')
+  })
 })
