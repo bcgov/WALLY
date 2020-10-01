f713f8710548fa496cb7bd05bc9236f30cf5e59b
AlexZorkin <alex@bigthink.solutions>
2020-10-01 07:31:36 -0700

    move wally model

--- frontend/src/components/analysis/surface_water/ComparitiveRunoffModels.vue
+++ frontend/src/components/analysis/surface_water/ComparitiveRunoffModels.vue
@@ -5,6 +5,38 @@
       dark>
       Comparative Runoff Models
     </v-card-title>
+    <v-card-text v-if="showWallyModelFeatureFlag && hydrologicalZoneModelRunoff">
+      <v-card-actions>
+        <v-card-subtitle class="pr-0 pl-2 pr-2">
+          Source:
+        </v-card-subtitle>
+        Wally Hydrological Zone Model
+      </v-card-actions>
+      <v-row class="pl-3 pr-3">
+        <v-col>
+          <v-card flat outlined tile height="100%">
+            <v-card-title>
+              Mean Annual Runoff Estimate:
+              <v-icon small class="ml-1">mdi-information-outline</v-icon>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <strong>{{ hydrologicalZoneModelRunoff }} m^3/sec</strong>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card flat outlined tile height="100%">
+            <v-card-title>
+              Model R Squared:
+              <v-icon small class="ml-1">mdi-information-outline</v-icon>
+            </v-card-title>
+            <v-card-text class="info-blue">
+              <strong>{{ hydrologicalZoneModelRSquared }}</strong>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card-text>
     <v-card-text v-if="annualNormalizedRunoff">
       <v-card-actions>
         <v-card-subtitle class="pr-0 pl-2 pr-2">
@@ -99,39 +131,6 @@
               :layout="isolineRunoffLayout"
               :data="isolineRunoffByMonth">
       </Plotly>
-    </v-card-text>
-
-<v-card-text v-if="hydrologicalZoneModelRunoff">
-      <v-card-actions>
-        <v-card-subtitle class="pr-0 pl-2 pr-2">
-          Source:
-        </v-card-subtitle>
-        Wally Hydrological Zone Model
-      </v-card-actions>
-      <v-row class="pl-3 pr-3">
-        <v-col>
-          <v-card flat outlined tile height="100%">
-            <v-card-title>
-              Mean Annual Runoff Estimate:
-              <v-icon small class="ml-1">mdi-information-outline</v-icon>
-            </v-card-title>
-            <v-card-text class="info-blue">
-              <strong>{{ hydrologicalZoneModelRunoff }} m^3/sec</strong>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card flat outlined tile height="100%">
-            <v-card-title>
-              Model R Squared:
-              <v-icon small class="ml-1">mdi-information-outline</v-icon>
-            </v-card-title>
-            <v-card-text class="info-blue">
-              <strong>{{ hydrologicalZoneModelRSquared }}</strong>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
     </v-card-text>
     <v-card-text v-else-if="!fishLoading">
       <p class="text--disabled mt-2">Unknown fish presence</p>
@@ -367,6 +366,9 @@ export default {
     }
   },
   methods: {
+    showWallyModelFeatureFlag () {
+      return this.app && this.app.config && this.app.config.wally_model
+    }
   },
   mounted () {
   }
