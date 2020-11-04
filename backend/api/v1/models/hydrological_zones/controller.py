eccd26123dd8a84667b30f2a520713f774be7fb9
AlexZorkin <alex@bigthink.solutions>
2020-11-03 22:10:49 -0800

    logging test

--- backend/api/v1/models/hydrological_zones/controller.py
+++ backend/api/v1/models/hydrological_zones/controller.py
@@ -185,9 +185,15 @@ def get_set_model_data(minio_path: str, file_name: str):
     """
     Gets a model object from Minio and sets the file by file_name
     """
+    logger.warning(minio_path)
+    logger.warning(file_name)
     try:
         response = minioClient.get_object('models', minio_path)
+        logger.warning(response)
+
         content = response.read().decode('utf-8')
+        logger.warning(content)
+
         with open(file_name, "w") as local_file:
             local_file.write(content)
     except Exception as error:
