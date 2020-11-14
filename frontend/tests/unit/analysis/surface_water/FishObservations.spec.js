fe3602aa57c52431ce955016d89435b90c2ad9e6
daine <daine.trinidad@gov.bc.ca>
2020-11-13 16:03:54 -0800

    New surface water tests

---
+++ frontend/tests/unit/analysis/surface_water/FishObservations.spec.js
@@ -0,0 +1,49 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import FishObservations
+  from '../../../../src/components/analysis/surface_water/FishObservations'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+// localVue with Vuetify shows console warnings so we'll use Vue instead
+// https://github.com/vuetifyjs/vuetify/issues/4964
+// localVue.use(Vuetify)
+Vue.use(Vuetify)
+
+const vuetify = new Vuetify()
+
+describe('Fish Observation tests', () => {
+  let wrapper
+  let store
+
+  beforeEach(() => {
+    console.error = jest.fn()
+    let map = {
+      namespaced: true
+    }
+
+    store = new Vuex.Store({ modules: { map } })
+
+    store.dispatch = jest.fn()
+    wrapper = shallowMount(FishObservations, {
+      vuetify,
+      store,
+      localVue,
+      propsData: {
+        watershedID: '',
+        surface_water_design_v2: true
+      }
+    })
+  })
+  it('Toggles layer visibility', () => {
+    console.log(wrapper)
+    wrapper.vm.isFishLayerVisible = true
+    wrapper.vm.toggleLayerVisibility()
+    expect(store.dispatch).toHaveBeenCalledWith(
+      'map/removeMapLayer',
+      'fish_observations')
+  })
+})
