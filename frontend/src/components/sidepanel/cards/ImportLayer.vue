78684f44952637ca180de46a262c6cadae42e602
daine <daine.trinidad@gov.bc.ca>
2020-12-07 19:42:31 -0800

    Add shapefile to instructions

--- frontend/src/components/sidepanel/cards/ImportLayer.vue
+++ frontend/src/components/sidepanel/cards/ImportLayer.vue
@@ -8,11 +8,12 @@
     <v-row class="pa-5">
       <v-col>
         <p>Choose files, data or layers to import from your computer, which will become temporarily available on the map.</p>
-        <p>Supported file types include: geojson, csv, xlsx, kml</p>
+        <p>Supported file types include: geojson, csv, xlsx, kml, shapefiles (shp, dbf, prj)</p>
         <p>Imported files require a location in order to be displayed. The supported coordinated system is decimal degrees longitude/latitude (WGS84).
             For example, -127.10205, 51.81051</p>
         <p>For CSV and Excel, the files should have two columns with the headings (not case sensitive): "Latitude" and "Longitude" or "lat" and "long".
             It works best to upload an Excel workbook that has a table on the first sheet (and no other cells filled in outside the table).</p>
+        <p>Shapefiles must include both the .shp and .prj. Uploading the .dbf is also recommended if you want to view features and properties of the layer.</p>
         <p>Large or complex files may impact performance. A message will indicate if the file has been successfully uploaded.</p>
       </v-col>
     </v-row>
