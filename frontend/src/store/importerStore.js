eb0a982b0f66ef150cee71da22349c9b4778adae
dainetrinidad <daine.trinidad@gov.bc.ca>
2020-12-14 09:43:20 -0800

    Cleanup debugging code

--- frontend/src/store/importerStore.js
+++ frontend/src/store/importerStore.js
@@ -17,7 +17,6 @@ export default {
       }
 
       let filenamesStr = filenames.filter(Boolean).join(', ')
-      console.log('processFile', filenamesStr, filenames)
 
       let processedFile = {
         name: filenamesStr,
@@ -43,17 +42,10 @@ export default {
       state.queuedFiles.push(file)
     },
     stopLoadingFile (state, filename) {
-      console.log('delete from loading file', filename, state.loadingFiles)
       state.loadingFiles = state.loadingFiles.filter(x => x !== filename)
-      console.log(state.loadingFiles)
     },
     startLoadingFile (state, filename) {
-      console.log('set loading for', filename, state.loadingFiles)
-      // state.loadingFiles[filename] = loading
-      // state.loadingFiles = { ...state.loadingFiles, filename: loading }
       state.loadingFiles.push(filename)
-
-      console.log(state.loadingFiles)
     },
     setProcessedFile (state, { status, processedFile }) {
       state.processedFiles[status].push(processedFile)
