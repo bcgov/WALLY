2e4490f5ef6b25d6a23f50c9bbea519d9a1c7323
dainetrinidad <daine.trinidad@gov.bc.ca>
2021-01-11 15:00:46 -0800

    Update css/scss loaders

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,10 +1,88 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",    "@storybook/addon-essentials"
+const path = require('path') // used for resolving aliases
 
-  ]
+module.exports = {
+  'stories': [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
+  'addons': [
+    '@storybook/addon-links', '@storybook/addon-essentials',
+    '@storybook/preset-scss'
+  ],
+
+  // add this function to tweak the webpack config
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook
+
+    // so I can import { storyFactory } from '~storybook/util/helpers'
+    // config.resolve.alias['~storybook'] = path.resolve(__dirname)
+    // the @ alias points to the <code>src/</code> directory, a common alias
+    // used in the Vue community
+    config.resolve.alias['@'] = path.resolve(__dirname, '..', 'src')
+
+    // config.module.rules.push({
+    //   test: /\.sass$/,
+    //   use: [
+    //     'vue-style-loader',
+    //     'css-loader',
+    //     {
+    //       loader: 'sass-loader',
+    //       // Requires sass-loader@^7.0.0
+    //       options: {
+    //         // This is the path to your variables
+    //         data: "@import '@/styles/variables.scss'"
+    //       },
+    //       // Requires sass-loader@^8.0.0
+    //       options: {
+    //         // This is the path to your variables
+    //         prependData: "@import '@/styles/variables.scss'"
+    //       },
+    //       // Requires sass-loader@^9.0.0
+    //       options: {
+    //         // This is the path to your variables
+    //         additionalData: "@import '@/styles/variables.scss'"
+    //       },
+    //     },
+    //   ],
+    // })
+
+    // -- from https://morphatic.com/2020/09/30/configuring-storybook-6-for-vue-2-vuetify-2-3/
+    // THIS is the tricky stuff!
+    // config.module.rules.push({
+    //   test: /\.sass$/,
+    //   use: [
+    //     'style-loader',
+    //     'css-loader',
+    //     {
+    //       loader: 'sass-loader',
+    //       options: {
+    //         sassOptions: {
+    //           indentedSyntax: true,
+    //         },
+    //         prependData: `@import '@/sass/variables.sass'`,
+    //       },
+    //     }
+    //   ],
+    //   include: path.resolve(__dirname, '../'),
+    // })
+
+    // config.module.rules.push({
+    //   test: /\.scss$/,
+    //   use: ['style-loader', 'css-loader', 'sass-loader'],
+    //   include: path.resolve(__dirname, '../'),
+    // });
+
+    // return the updated Storybook configuration
+    return config
+  },
+  // presets: [
+  //   'vue/app'
+  // ],
 }
+//   ],
+//   presets: [
+//     '@vue/app'
+//   ],
+// }
--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,46 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { options } from '../src/plugins/vuetify'// <== important
+// configure Vue to use Vuetify
+Vue.use(Vuetify)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// export const decorators = [() => ({
+//   withVuetify,
+//   template: '<div style="margin: 3em;"><story /></div>' })];
+
+// instantiate Vuetify instance with any component/story level params
+const vuetify = new Vuetify(options)
+
+export const decorators = [
+  (story, context) => {
+
+
+    console.log('vuetify', options, vuetify)
+    // wrap the passed component within the passed context
+    const wrapped = story(context)
+    // extend Vue to use Vuetify around the wrapped component
+    return Vue.extend({
+        vuetify,
+        components: { wrapped },
+        props: {
+          dark: {
+            type: Boolean,
+            default: context.args.dark,
+          }
+        },
+        watch: {
+          dark: {
+            immediate: true,
+            handler (val) {
+              this.$vuetify.theme.dark = val
+            }
+          }
+        },
+        template: '<code> <v-app> <v-container fluid> <wrapped /> </v-container> </v-app> </code>'
+  })
+  },
+]
