5918ed3699a5712d1a3d76c20c66f28017ce73b7
Daine Trinidad <daine.trinidad@gov.bc.ca>
2021-03-02 11:24:37 -0800

    Add util for getting user

--- backend/api/v1/user/routes.py
+++ backend/api/v1/user/routes.py
@@ -4,19 +4,21 @@ User management endpoints
 import os
 from logging import getLogger
 from typing import List, Optional
-from fastapi import APIRouter, Depends, Header
+from fastapi import APIRouter, Depends, Header, Request
 from sqlalchemy.orm import Session
 from api.db.utils import get_db
 from api.v1.user.schema import User
+from api.v1.user.session import get_user
 import api.v1.user.controller as controller
 
-logger = getLogger("catalogue")
+logger = getLogger("user")
 
 router = APIRouter()
 
 
 @router.get("/profile")
 def get_create_user_profile(
+        user: User = Depends(get_user),
         x_auth_userid: Optional[str] = Header(None),
         db: Session = Depends(get_db)
 ):
@@ -27,6 +29,7 @@ def get_create_user_profile(
     header up from the proxy service (X-Auth-UserId)
     """
     # TODO: Change this to return a user and not a user map layer once user migration is complete
+    # User uuid is accessible through user.user_uuid
     return controller.get_create_user_map_layer(db, x_auth_userid)
 
 
