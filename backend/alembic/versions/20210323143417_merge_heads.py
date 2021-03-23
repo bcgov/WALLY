c01b6d446655f88826b234ffa937e5bd72aaa802
AlexZorkin <alex@bigthink.solutions>
2021-03-23 14:35:36 -0700

    merge alembic heads

---
+++ backend/alembic/versions/20210323143417_merge_heads.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a691dfe51337
+Revises: c82f8499050e, 48ca44a2a61f
+Create Date: 2021-03-23 14:34:17.027909
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a691dfe51337'
+down_revision = ('c82f8499050e', '48ca44a2a61f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
