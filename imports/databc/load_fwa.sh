cc9fa428c44f3331edc98e9faeb654a7124dd872
Steve Hillier <stephenhillier@gmail.com>
2021-04-26 11:06:46 -0700

    add FWA loader script

---
+++ imports/databc/load_fwa.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# USAGE: ./load_fwa.sh <layer_name>
+# Load Freshwater Atlas data.
+# Freshwater Atlas streams and watersheds come in large files (GeoDatabase .GDB files) and must
+# be loaded with this script instead of load_layer_data.
+#
+# use only the layer name. The layer name MUST be the layer name as spelled in the Wally database table.
+# check the db table name or the __tablename__ of the SQLAlchemy model in the `wally/backend/api/layers` files.
+# There should be an identically spelled <layer_name>.zip file available on the S3 storage configured below.
+# The environment must have the following env variables:
+# MINIO_ACCESS_KEY
+# MINIO_SECRET_KEY
+# POSTGRES_SERVER
+# POSTGRES_PASSWORD
+# The database name is assumed to be "wally".
+
+set -e
+cd /dataload
+echo "Setting up Minio host"
+./mc --config-dir=./.mc config host add minio "$MINIO_HOST_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+
+echo "Copying layer from Minio storage..."
+./mc --config-dir=./.mc cp "minio/geojson/$1.zip" "./"
+
+echo "Loading data with ogr2ogr"
+ogr2ogr -f "PostgreSQL" PG:"host=$POSTGRES_SERVER port=5432 dbname=wally user=wally password=$POSTGRES_PASSWORD" "/vsizip/$1.zip/$1.gdb" \
+-nln "$1" -t_srs EPSG:4326 -append -progress -skipfailures --config OGR_TRUNCATE YES --config PG_USE_COPY YES
+
+echo "Finished loading data. Updating Wally metadata table".
+
+# get the date this data was last updated.  IMPORTANT: this is inferred by the Last Modified date on the source data from S3 storage.
+# NOT the time when the data was loaded into the database.  This is to help prevent the date updated column being set to "now()" if
+# the data being used is somehow out of date.
+last_updated=$(./mc --config-dir=./.mc ls "minio/geojson/$1.zip" --json | jq .lastModified)
+
+psql "postgres://wally:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/wally" -c "UPDATE metadata.data_source SET last_updated_data = '$last_updated' WHERE data_table_name = '$1';"
+
+echo "Finished."
+
+
+
+ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 dbname=wally user=wally password=test_pw" /vsizip/$1.zip/$1.gdb \
+-nln freshwater_atlas_watersheds -t_srs EPSG:4326 -append -progress -skipfailures --config OGR_TRUNCATE YES --config PG_USE_COPY YES
