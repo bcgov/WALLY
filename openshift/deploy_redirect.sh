fc48b3f65e4744592e691938681a53ca8707406e
Daine Trinidad <daine.trinidad@gov.bc.ca>
2021-04-27 09:10:57 -0700

    Change proxy to redirect, change URLs

--- openshift/deploy_redirect.sh
+++ openshift/deploy_redirect.sh
@@ -2,20 +2,25 @@
 # Usage: ./deploy_redirect.sh <namespace> <environment>
 # Sample usage: 
 #    ./deploy_redirect.sh bfpeyx-dev dev
-#    ./deploy_redirect.sh bfpeyx-staging staging
+#    ./deploy_redirect.sh bfpeyx-test staging
 #    ./deploy_redirect.sh bfpeyx-prod
 
 PROJECT=${1:-}
 ENV=${2:-}
-NAME_SUFFIX="-$2"
-DC_TMPL=redirect.dc.yaml
 
 # Destination will be the URL in the silver cluster
-# Will change this to wally${NAME_SUFFIX}.apps.silver.devops.gov.bc.ca
-DEST_URL=apps.nrs.gov.bc.ca
+DEST_URL=wally.nrs.gov.bc.ca
+NAME_SUFFIX=
+
+if [ "$ENV" != 'prod' ]; then
+  NAME_SUFFIX="-$2"
+  DEST_URL=wally${NAME_SUFFIX}.apps.silver.devops.gov.bc.ca
+fi
+DC_TMPL=redirect.dc.yaml
+
 
 # Source is the URL from pathfinder
 SOURCE_URL=wally-redirect${NAME_SUFFIX}.pathfinder.gov.bc.ca
 
 # Process deployment config
-oc -n $PROJECT process -f $DC_TMPL -p NAME_SUFFIX=$NAME_SUFFIX DESTINATION_HOST_NAME=$DEST_URL SOURCE_HOST_NAME=$SOURCE_URL | oc apply -f - 
+oc -n $PROJECT process -f $DC_TMPL -p NAME_SUFFIX=$NAME_SUFFIX DESTINATION_HOST_NAME=$DEST_URL SOURCE_HOST_NAME=$SOURCE_URL | oc -n $PROJECT apply -f - 
