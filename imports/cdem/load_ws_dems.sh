5a16c2e7b893518ccbc483eb9200ca326270cbca
Steve Hillier <stephenhillier@gmail.com>
2021-04-29 11:20:55 -0700

    update dem loader to run as job

--- imports/cdem/load_ws_dems.sh
+++ imports/cdem/load_ws_dems.sh
@@ -5,18 +5,14 @@
 
 set -euo pipefail
 mkdir -p /waterdata/rasters
+cdem_file="Burned_CDEM_BC_Area_4326.tif"
+srtm_file="Burned_SRTM_BC_48_51_3005.tif"
 
 echo "(1/3) Setting up Minio host"
-mc --config-dir=./.mc config host add minio ${MINIO_HOST_URL} "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+mc --config-dir=./.mc config host add minio "${MINIO_HOST_URL}" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
-if [ ! -f "/waterdata/rasters/Burned_CDEM_BC_Area_4326.tif" ]; then
-    echo "Burned CDEM file not found!"
-    mc --config-dir=./.mc cp "minio/raster/Burned_CDEM_BC_Area_4326.tif" "/waterdata/rasters"
-    echo "Burned CDEM loaded to /waterdata/rasters/Burned_CDEM_BC_Area_4326.tif"
-fi
+mc --config-dir=./.mc cp "minio/raster/$cdem_file" "/waterdata/rasters"
+echo "Burned CDEM loaded to /waterdata/rasters/$cdem_file"
 
-if [ ! -f "/waterdata/rasters/Burned_SRTM_BC_48_51_3005.tif" ]; then
-    echo "Burned SRTM file not found!"
-    mc --config-dir=./.mc cp "minio/raster/Burned_SRTM_BC_48_51_3005.tif" "/waterdata/rasters"
-    echo "Burned SRTM loaded to /waterdata/rasters/Burned_SRTM_BC_48_51_3005.tif"
-fi
+mc --config-dir=./.mc cp "minio/raster/$srtm_file" "/waterdata/rasters"
+echo "Burned SRTM loaded to /waterdata/rasters/$srtm_file"
