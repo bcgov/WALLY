5f662ceb8fd8b76bd15bdea684964ba59e35e0ec
Steve Hillier <stephenhillier@gmail.com>
2021-06-15 14:59:40 -0700

    update load_hydat script

--- imports/hydat/load_hydat.sh
+++ imports/hydat/load_hydat.sh
@@ -1,14 +1,76 @@
 #!/bin/bash
-curl https://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/Hydat_sqlite3_20210116.zip -o /tmp/hydat.zip && \
-unzip /tmp/hydat.zip -d /tmp && \
-mkdir -p /dataload/pgloader && \
+
+LATEST_HYDAT="${LATEST_HYDAT:-Hydat_sqlite3_20210510}"
+cd /tmp
+
+if [ ! -f "/tmp/$LATEST_HYDAT.zip" ]; then
+    curl "https://collaboration.cmc.ec.gc.ca/cmc/hydrometrics/www/$LATEST_HYDAT.zip" -O
+fi
+
+unzip /tmp/$LATEST_HYDAT.zip -d /tmp && \
+mkdir -p /tmp/pgloader && \
 pgloader \
-    -L /dataload/pgloader/pgloader.log -D /dataload/pgloader \
+    -L /tmp/pgloader/pgloader.log -D /tmp/pgloader \
     --type sqlite \
     --with "create no tables" \
     --with "truncate" \
     --set "search_path='hydat'" \
     /tmp/Hydat.sqlite3 \
-    postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/$POSTGRES_DB
+    postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/$POSTGRES_DB && \
+rm "/tmp/$LATEST_HYDAT.zip" && \
+rm /tmp/Hydat.sqlite3 && \
+psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/$POSTGRES_DB" -w -c "update hydat.stations set geom=ST_SetSrid(ST_MakePoint(longitude, latitude), 4326);" && \
 
-psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/$POSTGRES_DB" -w -c "update hydat.stations set geom=ST_SetSrid(ST_MakePoint(longitude, latitude), 4326);"
+# transform HYDAT data into something resembling time series (station : date : value)
+# and load into the `hydat.fasstr_flows`
+psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER:5432/$POSTGRES_DB" << EOF
+  with flows as (
+    select
+      f.station_number,
+      year,
+      month,
+      flow1,
+      flow2,
+      flow3,
+      flow4,
+      flow5,
+      flow6,
+      flow7,
+      flow8,
+      flow9,
+      flow10,
+      flow11,
+      flow12,
+      flow13,
+      flow14,
+      flow15,
+      flow16,
+      flow17,
+      flow18,
+      flow19,
+      flow20,
+      flow21,
+      flow22,
+      flow23,
+      flow24,
+      flow25,
+      flow26,
+      flow27,
+      flow28,
+      flow29,
+      flow30,
+      flow31
+    from hydat.dly_flows f
+    inner join hydat.stations s on s.station_number = f.station_number
+    where s.prov_terr_state_loc = 'BC'
+  ),
+  kv as (
+    select station_number, year, month, each(hstore(flows)) as kv from flows
+  )
+  insert into hydat.fasstr_flows (station_number, date, value)
+  select
+    station_number,
+    to_date(concat(year::text, lpad(month::text, 2, '0'), lpad(replace((kv).key, 'flow', '')::text, 2, '0')), 'YYYYMMDD') as date,
+    (kv).value::numeric as value
+  from kv where (kv).key like 'flow%' and (kv).value is not null;
+EOF
