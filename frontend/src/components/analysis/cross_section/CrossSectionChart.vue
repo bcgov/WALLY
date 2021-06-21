7c0560ca68846b128b89c60493d2b1917fb4da80
Daine Trinidad <daine.trinidad@gov.bc.ca>
2021-06-21 14:38:15 -0700

    Bring back reset labels button

--- frontend/src/components/analysis/cross_section/CrossSectionChart.vue
+++ frontend/src/components/analysis/cross_section/CrossSectionChart.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <Plotly v-show="!dataLoading" id="2dPlot" :data="chartData" :layout="chartLayout"  :modeBarButtonsToRemove="ignoreButtons" :displayModeBar="true" ref="crossPlot"></Plotly>
+    <v-btn small v-on:click="resetMarkerLabels" color="blue-grey lighten-4" class="mb-1 mt-0 mr-5 ml-2">
+      <span class="hidden-sm-and-down"><v-icon color="secondary" class="mr-1" size="18">format_clear</v-icon>Reset Labels</span>
+    </v-btn>
+    <Plotly v-show="!dataLoading" :id="plotId" :data="chartData" :layout="chartLayout" :modeBarButtonsToRemove="ignoreButtons" :displayModeBar="true" ref="crossPlot"></Plotly>
   </div>
 </template>
 <script src="./CrossSectionChart.js"></script>
