229e5b5e01efdfebfa551319b4e0f3bf7bddade1
Steve Hillier <stephenhillier@gmail.com>
2021-06-22 13:00:11 -0700

    remove unused test (testing prop that wasn't itself used)

--- frontend/tests/unit/features/StreamStation.spec.js
+++ frontend/tests/unit/features/StreamStation.spec.js
@@ -20,26 +20,4 @@ describe('StreamStation.vue', () => {
     })
     expect(wrapper.find('#stationTitle').text()).toMatch('station name')
   })
-  it('generates chartOptions with x and y axis', () => {
-    const wrapper = shallowMount(StreamStation, {
-      vuetify,
-      localVue,
-      propsData: {
-        record: {
-          properties: { name: 'station name', url: '/test' }
-        }
-      }
-    })
-    wrapper.setData(
-      {
-        flowChartOptions: wrapper.vm.newChartOptions(
-          'New chart',
-          'days', // unit
-          [1, 2, 3, 4]
-        )
-      }
-    )
-    expect(wrapper.vm.flowChartOptions.scales.yAxes.length).toEqual(1)
-    expect(wrapper.vm.flowChartOptions.scales.xAxes.length).toEqual(1)
-  })
 })
