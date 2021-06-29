cffc81eb001849e5f079934624582c6fa655ca3b
AlexZorkin <alex@bigthink.solutions>
2021-06-29 14:11:23 -0700

    leap year feb seconds

--- frontend/src/constants/months.js
+++ frontend/src/constants/months.js
@@ -2,6 +2,10 @@ export const daysInMonth = { 1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8:
 export const secondsInDay = 86400
 export const secondsInYear = 31536000
 
-export const secondsInMonth = (month) => {
-  return daysInMonth[month] * secondsInDay
+export const secondsInMonth = (month, leapYear = false) => {
+  let seconds = daysInMonth[month] * secondsInDay
+  if (month === 2 && leapYear) {
+    seconds += secondsInDay
+  }
+  return seconds
 }
