6453dbbaf2b6c14753b0bb697691ec61c76f5217
AlexZorkin <alex@bigthink.solutions>
2021-06-30 16:57:13 -0700

    jun30 updates

--- modelling/v3/scripts/flatten_mean_annual_flows.py
+++ modelling/v3/scripts/flatten_mean_annual_flows.py
@@ -16,7 +16,7 @@ for filename in sorted(os.listdir(directory)):
     if filename.endswith(".csv"):
         # zone_name = filename.split('_')[1].split('.')[0]
         
-        zone_df = pd.read_csv(os.path.join(directory, 'bc_mean_annual_flows_unflattened.csv'))
+        zone_df = pd.read_csv(os.path.join(directory, 'jun30.csv'))
         station_groups = zone_df.groupby("STATION_NUMBER")
         # print(station_groups.size())
         
@@ -45,6 +45,6 @@ for filename in sorted(os.listdir(directory)):
         # print("ten year stations:", ten_year_stations.count())
         # print("twenty year stations:", twenty_year_stations.count())
 
-        ten_year_stations.to_csv(directory + "yearly_stations/bc_mean_annual_flows_10_year_stations.csv", index=False)
-        twenty_year_stations.to_csv(directory + "yearly_stations/bc_mean_annual_flows_20_year_stations.csv", index=False)
-        flattened_years.to_csv(directory + "bc_mean_annual_flows.csv", index=False)
+        # ten_year_stations.to_csv(directory + "yearly_stations/bc_mean_annual_flows_10_year_stations.csv", index=False)
+        # twenty_year_stations.to_csv(directory + "yearly_stations/bc_mean_annual_flows_20_year_stations.csv", index=False)
+        flattened_years.to_csv(directory + "bc_mean_annual_flows_jun30.csv", index=False)
--- modelling/v3/scripts/percentdiscrep_marconvert_splitintozones.py
+++ modelling/v3/scripts/percentdiscrep_marconvert_splitintozones.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 
 # import data
-df = pd.read_csv("../data/2_scrape_results/watershed_stats_output_06_15_2021-16_47_07.csv")
+df = pd.read_csv("../data/2_scrape_results/watershed_stats_output_06_30_2021-23_38_57.csv")
 
 # 5 percent discrepancy
 indexNames = df[((df['drainage_area'] / df['drainage_area_gross']) - 1).abs() >= 0.2].index
@@ -14,7 +14,7 @@ df['mean'] = (df['mean'] / df['drainage_area']) * 1000
 print(df[['station_number','latitude', 'longitude', 'drainage_area', 'drainage_area_gross']])
 
 # export to files
-# output_directory = "jun15"
+output_directory = "jun30"
 
-# for zone, rows in df.groupby('hydrological_zone'):
-#     rows.to_csv(f'../data/4_training/{output_directory}/{round(zone)}.csv', index=False, header=True)
+for zone, rows in df.groupby('hydrological_zone'):
+    rows.to_csv(f'../data/4_training/{output_directory}/{round(zone)}.csv', index=False, header=True)
