3b8dbf9efdfa3a5cdf1cff0d46ab6e53b0848223
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2023-06-15 12:30:02 -0700

    Change Access Token request
    Changing way access token is requested to match the way this is done through postman tests. Works in postman but is failing in app.

--- backend/external/docgen/request_token.py
+++ backend/external/docgen/request_token.py
@@ -1,25 +1,29 @@
 import requests
+import base64
 from api import config
-
 
 def get_docgen_token():
     params = {
         "grant_type": "client_credentials",
-        "client_id": config.COMMON_DOCGEN_CLIENT_ID,
-        "client_secret": config.COMMON_DOCGEN_CLIENT_SECRET,
         "scope": ""
+    }
+
+    auth_string = f"{config.COMMON_DOCGEN_CLIENT_ID}:{config.COMMON_DOCGEN_CLIENT_SECRET}"
+    encoded_auth = base64.b64encode(auth_string.encode()).decode()
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {encoded_auth}"
     }
 
     req = requests.post(
         config.COMMON_DOCGEN_SSO_ENDPOINT,
         data=params,
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
+        headers=headers
     )
     req.raise_for_status()
 
     resp = req.json()
 
-    token = req.json().get('access_token')
+    token = resp.get('access_token')
     return token
