cb68bd549a90bb644e071f0ba728a1dd4e98e7c1
David <62899351+davidclaveau@users.noreply.github.com>
2023-06-20 14:23:05 -0700

    update how we request cdogs access token

--- backend/external/docgen/request_token.py
+++ backend/external/docgen/request_token.py
@@ -1,25 +1,35 @@
 import requests
+import base64
 from api import config
+from fastapi import HTTPException
+import logging
 
+logger = logging.getLogger('docgen')
 
 def get_docgen_token():
     params = {
-        "grant_type": "client_credentials",
-        "client_id": config.COMMON_DOCGEN_CLIENT_ID,
-        "client_secret": config.COMMON_DOCGEN_CLIENT_SECRET,
-        "scope": ""
+        "grant_type": "client_credentials"
     }
 
-    req = requests.post(
-        config.COMMON_DOCGEN_SSO_ENDPOINT,
-        data=params,
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    )
-    req.raise_for_status()
+    auth_string = f"{config.COMMON_DOCGEN_CLIENT_ID}:{config.COMMON_DOCGEN_CLIENT_SECRET}"
+    encoded_auth = base64.b64encode(auth_string.encode()).decode()
 
-    resp = req.json()
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Basic {encoded_auth}"
+    }
 
-    token = req.json().get('access_token')
-    return token
+    logger.info('making POST request to common docgen sso endpoint: %s',
+                config.COMMON_DOCGEN_SSO_ENDPOINT)
+
+    try:
+        res = requests.post(
+            config.COMMON_DOCGEN_SSO_ENDPOINT,
+            data=params,
+            headers=headers)
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logger.info(e)
+        raise HTTPException(status_code=e.response.status_code, detail=str(e))
+
+    return res.json().get('access_token')
