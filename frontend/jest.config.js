63a310a336b3b80a76376fde156bad51512be482
Ariel Rotter-Aboyoun <raarielgrace@gmail.com>
2025-03-12 16:20:25 -0700

    9/33 unit tests passing

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
+  testEnvironment: 'jsdom',
   transformIgnorePatterns: [
     '/node_modules/(?!(@mapbox/mapbox-gl-draw)/)'
   ],
